c16bfd7ee12c62a4e07e3a10b275a312e2126bf6
dmalnati <douglas.malnati@gmail.com>
2016-04-17 20:50:29 -0400

    AVR LCDFrentaly20x4 working

---
+++ libraries/HID/LCDFrentaly20x4.h
@@ -0,0 +1,58 @@
+#ifndef __LCD_FRENTALY_20X4_H__
+#define __LCD_FRENTALY_20X4_H__
+
+
+#include "LiquidCrystal_I2C.h"
+
+
+class LCDFrentaly20x4
+{
+    static const uint8_t I2C_ADDR  = 0x3F;
+    static const uint8_t COL_COUNT = 20;
+    static const uint8_t ROW_COUNT = 4;
+    
+public:
+    LCDFrentaly20x4()
+    : lcd_(I2C_ADDR, COL_COUNT, ROW_COUNT)
+    {
+        // Nothing to do
+    }
+    
+    void Init()
+    {
+        lcd_.begin();
+        lcd_.backlight();
+    }
+    
+    template <typename T>
+    uint8_t Print(T &&t)
+    {
+        uint8_t charCount = lcd_.print(t);
+        
+        return charCount;
+    }
+    
+    template <typename T>
+    uint8_t PrintAt(uint8_t col, uint8_t row, T &&t)
+    {
+        lcd_.setCursor(col, row);
+        
+        return Print(t);
+    }
+    
+    void Clear()
+    {
+        lcd_.clear();
+    }
+    
+
+private:
+
+    LiquidCrystal_I2C lcd_;
+};
+
+
+
+#endif  // __LCD_FRENTALY_20X4_H__
+
+
---
+++ sketches/TestLCDFrentaly20x4/TestLCDFrentaly20x4.ino
@@ -0,0 +1,37 @@
+#include "LCDFrentaly20x4.h"
+
+static LCDFrentaly20x4 lcd;
+
+void setup()
+{
+    lcd.Init();
+
+    lcd.PrintAt(0, 0, "01234567890123456789");
+    lcd.PrintAt(0, 1, "|   Hello          |");
+    lcd.PrintAt(0, 2, "|         World!   |");
+    lcd.PrintAt(0, 3, "01234567890123456789");
+    
+    char spinCharList[] = { '^', '>', 'v', '<' };
+    uint8_t spinCharListLen = sizeof(spinCharList);
+    uint8_t idx = 0;
+    
+    while (1)
+    {
+        idx = (idx + 1) % spinCharListLen;
+        
+        char c = spinCharList[idx];
+        
+        lcd.PrintAt( 0, 1, c);
+        lcd.PrintAt(19, 1, c);
+        
+        lcd.PrintAt( 0, 2, c);
+        lcd.PrintAt(19, 2, c);
+        
+        delay(250);
+    }
+}
+
+void loop() { }
+
+
+
