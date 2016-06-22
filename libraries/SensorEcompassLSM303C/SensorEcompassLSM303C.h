5fdcd5148e1ed58d34ddf1dc4ac2385eb236a4aa
dmalnati <douglas.malnati@gmail.com>
2016-06-21 22:24:51 -0400

    AVR SensorEcompassLSM303C working

--- libraries/SensorEcompassLSM303C/SensorEcompassLSM303C.h
+++ libraries/SensorEcompassLSM303C/SensorEcompassLSM303C.h
@@ -2,6 +2,10 @@
 #define __ECOMPASS_LSM303C_H__
 
 
+// This set of files has tons of compilation warnings (not errors)
+// (warning: narrowing conversion of '+QNaN' from 'double' to 'int16_t {aka int}' inside { } [-Wnarrowing])
+// (warning: overflow in implicit constant conversion [-Woverflow])
+// Thanks SparkFun.
 #include "SparkFunLSM303C.h"
 
 
@@ -13,25 +17,51 @@ public:
     
     void Init()
     {
-        
+        sensor_.begin();
     }
     
     struct Measurement
     {
+        // Units of G
+        float accelX;
+        float accelY;
+        float accelZ;
         
+        // Units of Gauss
+        float magX;
+        float magY;
+        float magZ;
+        
+        float tempF;
     };
     
     uint8_t GetMeasurement(Measurement *m)
     {
-        uint8_t retVal = 0;
+        uint8_t retVal = 1;
         
+        // Re-begin on purpose.
+        // If power is lost to the sensor after initial begin, all Accel values
+        // become NaN.
+        // This call makes them resolve again at a cost of approx +2.5ms to
+        // a total of ~8ms.
+        sensor_.begin();
+        
+        m->accelX = sensor_.readAccelX() / 1000.0;
+        m->accelY = sensor_.readAccelY() / 1000.0;
+        m->accelZ = sensor_.readAccelZ() / 1000.0;
+        
+        m->magX   = sensor_.readMagX();
+        m->magY   = sensor_.readMagY();
+        m->magZ   = sensor_.readMagZ();
+        
+        m->tempF  = sensor_.readTempF();
         
         return retVal;
     }
 
 private:
 
-
+    LSM303C sensor_;
 };
 
 
--- sketches/TestSensorEcompassLSM303C/TestSensorEcompassLSM303C.ino
+++ sketches/TestSensorEcompassLSM303C/TestSensorEcompassLSM303C.ino
@@ -1,20 +1,63 @@
 #include "Evm.h"
+#include "Utl.h"
 #include "SensorEcompassLSM303C.h"
 
 
-
 static const uint32_t DEBUG_INTERVAL_MS = 1000;
+static const uint8_t  PIN_DEBUG_TOGGLE = 4;
 
 static Evm::Instance<10,10,10> evm;
 
 static TimedEventHandlerDelegate ted;
-static SensorEcompassLSM303C sensor();
+static SensorEcompassLSM303C sensor;
 
+void PrintMeasurement(SensorEcompassLSM303C::Measurement m)
+{
+    Serial.print("accelX: ");
+    Serial.print(m.accelX);
+    Serial.println();
+
+    Serial.print("accelY: ");
+    Serial.print(m.accelY);
+    Serial.println();
+    
+    Serial.print("accelZ: ");
+    Serial.print(m.accelZ);
+    Serial.println();
+    
+    Serial.print("magX: ");
+    Serial.print(m.magX);
+    Serial.println();
+
+    Serial.print("magY: ");
+    Serial.print(m.magY);
+    Serial.println();
+
+    Serial.print("magZ: ");
+    Serial.print(m.magZ);
+    Serial.println();
+    
+    Serial.print("tempF: ");
+    Serial.print(m.tempF);
+    Serial.println();
+
+    Serial.println();
+}
 
 void setup()
 {
-    ted.SetCallback([&]() {
+    Serial.begin(9600);
 
+    sensor.Init();
+    
+    ted.SetCallback([&]() {
+        SensorEcompassLSM303C::Measurement m;
+
+        PinToggle(PIN_DEBUG_TOGGLE, 0);
+        sensor.GetMeasurement(&m);
+        PinToggle(PIN_DEBUG_TOGGLE, 0);
+
+        PrintMeasurement(m);
     });
     
     ted.RegisterForTimedEventInterval(DEBUG_INTERVAL_MS);
