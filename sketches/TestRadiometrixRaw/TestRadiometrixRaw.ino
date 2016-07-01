b5a171f5a93986aa6b21615587d80180b9311bd7
dmalnati <douglas.malnati@gmail.com>
2016-07-01 01:04:54 -0400

    AVR TestRadiometrixRaw mid-testing checkin

--- sketches/TestRadiometrixRaw/TestRadiometrixRaw.ino
+++ sketches/TestRadiometrixRaw/TestRadiometrixRaw.ino
@@ -1,4 +1,6 @@
 #include "PAL.h"
+#include "Evm.h"
+#include "LedFader.h"
 
 static const uint8_t PIN_PWM = 15;
 
@@ -8,7 +10,7 @@ void TestStripe()
     
     while (1)
     {
-        PAL.AnalogWrite(PIN_PWM, val);
+        //PAL.AnalogWrite(PIN_PWM, val);
 
         PAL.Delay(10);
 
@@ -20,11 +22,11 @@ void TestFlipFlop()
 {
     while (1)
     {
-        PAL.AnalogWrite(PIN_PWM, 0);
+        //PAL.AnalogWrite(PIN_PWM, 0);
 
         PAL.Delay(1000);
 
-        PAL.AnalogWrite(PIN_PWM, 255);
+        //PAL.AnalogWrite(PIN_PWM, 255);
 
         PAL.Delay(1000);
     }
@@ -49,9 +51,66 @@ void TestTone()
     }
 }
 
+void TestFader()
+{
+    LEDFader<1,1> fader1;
+    Evm::Instance<10,10,10> evm;
+
+    fader1.AddLED(PIN_PWM);
+
+    while (1)
+    {
+        fader1.FadeOnce();
+
+        evm.HoldStackDangerously(1000);
+    }
+}
+
+void TestMaxToggle()
+{
+    Pin p(PIN_PWM);
+
+    while (1)
+    {
+        PAL.DigitalWrite(p, LOW);
+        delayMicroseconds(1);
+        PAL.DigitalWrite(p, HIGH);
+        delayMicroseconds(1);
+    }
+}
+
+void TestDelayResolution()
+{
+    uint16_t baud = 1200;
+    uint16_t usDelay = (uint16_t)((1000.0 / (double)baud) * 1000.0);
+
+    // usDelay = 833 (from 833.33333....)
+
+    Pin p(PIN_PWM);
+    
+    while (1)
+    {
+        PAL.DigitalWrite(p, LOW);
+        delayMicroseconds(usDelay);
+        PAL.DigitalWrite(p, HIGH);
+        delayMicroseconds(usDelay);
+    }
+}
+
+// this is worse than delayMicroseconds
+void MyDelay(uint16_t usDelay)
+{
+    uint32_t start = micros();
+    while (micros() - start < usDelay) { }
+}
+
 void setup()
 {
-    TestTone();
+    //TestFader();
+    //TestTone();
+    //TestMaxToggle();
+
+    TestDelayResolution();
 }
  
 void loop() { }
