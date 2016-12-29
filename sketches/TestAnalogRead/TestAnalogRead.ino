bf4686d4e2b4e8974bd40187a470700a1a2d382f
dmalnati <douglas.malnati@gmail.com>
2016-12-29 13:10:28 -0500

    AVR TestAnalogRead

---
+++ sketches/TestAnalogRead/TestAnalogRead.ino
@@ -0,0 +1,54 @@
+#include "PAL.h"
+
+
+
+struct PinState
+{
+    uint8_t  pin;
+    uint16_t oldVal;
+    uint16_t newVal;
+};
+
+static PinState pinStateList[] = {
+    { 23, 0, 0 },
+    { 24, 0, 0 },
+    { 25, 0, 0 },
+    { 26, 0, 0 },
+    { 27, 0, 0 },
+    { 28, 0, 0 }
+};
+
+void ReportAnalogRead(PinState &pinState)
+{
+    // move prior newVal to oldVal
+    pinState.oldVal = pinState.newVal;
+
+    // do read
+    pinState.newVal = PAL.AnalogRead(pinState.pin);
+
+    // decide if value changed
+    if (pinState.oldVal != pinState.newVal)
+    {
+        Serial.print(pinState.pin);
+        Serial.print(": ");
+        Serial.println(pinState.newVal);
+    }
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    while (1)
+    {
+        for (uint8_t i = 0; i < 6; ++i)
+        {
+            ReportAnalogRead(pinStateList[i]);
+        }
+    }
+}
+
+void loop() {}
+
+
+
