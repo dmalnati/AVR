4e006456b158557b1982eed3bf2055e836ae99ec
dmalnati <douglas.malnati@gmail.com>
2016-12-29 13:10:48 -0500

    AVR PinInputAnalog

---
+++ libraries/PAL/PinInputAnalog.h
@@ -0,0 +1,90 @@
+#ifndef __PIN_INPUT_ANALOG_H__
+#define __PIN_INPUT_ANALOG_H__
+
+
+#include "PAL.h"
+#include "Function.h"
+#include "TimedEventHandler.h"
+
+
+class PinInputAnalog
+: private TimedEventHandler
+{
+public:
+    static const uint8_t C_IDLE  = 0;
+    static const uint8_t C_TIMED = 1;
+    static const uint8_t C_INTER = 0;
+    
+private:
+    static const uint32_t DEFAULT_POLL_PERIOD_MS = 100;
+    static const uint16_t DEFAULT_MINIMUM_CHANGE = 15;
+    
+public:
+    PinInputAnalog(uint8_t  pin,
+                   uint32_t pollPeriodMs = DEFAULT_POLL_PERIOD_MS)
+    : pin_(pin)
+    , pollPeriodMs_(pollPeriodMs)
+    , minimumChange_(-1)
+    , valLast_(-1)
+    {
+        SetMinimumChange(DEFAULT_MINIMUM_CHANGE);
+    }
+    
+    void SetMinimumChange(uint16_t minimumChange)
+    {
+        minimumChange_ = minimumChange;
+    }
+    
+    void SetCallback(function<void(uint16_t val)> &&cbFn)
+    {
+        cbFn_ = cbFn;
+    }
+    
+    void Disable()
+    {
+        DeRegisterForTimedEvent();
+    }
+    
+    void Enable()
+    {
+        RegisterForTimedEventInterval(pollPeriodMs_);
+    }
+
+private:
+    virtual void OnTimedEvent()
+    {
+        uint16_t val = PAL.AnalogRead(pin_);
+        
+        if (minimumChange_ != -1)
+        {
+            // Fire when:
+            // - never fired before
+            // - changed by the minimum
+            // - hit either extreme of the spectrum of values
+            if (valLast_ == -1                        ||
+                abs(valLast_ - val) >= minimumChange_ ||
+                (val == 1023 && valLast_ != 1023)     ||
+                (val == 0    && valLast_ != 0))
+            {
+                cbFn_(val);
+                
+                valLast_ = val;
+            }
+        }
+        else
+        {
+            cbFn_(val);
+        }
+    }
+
+    uint8_t                      pin_;
+    uint32_t                     pollPeriodMs_;
+    int32_t                      minimumChange_;
+    int32_t                      valLast_;
+    function<void(uint16_t val)> cbFn_;
+};
+
+
+
+
+#endif  // __PIN_INPUT_ANALOG_H__
---
+++ sketches/TestPinInputAnalog/TestPinInputAnalog.ino
@@ -0,0 +1,40 @@
+#include "PAL.h"
+#include "Evm.h"
+#include "PinInputAnalog.h"
+
+
+static uint8_t PIN_ANALOG_1 = 28;
+static uint8_t PIN_ANALOG_2 = 27;
+
+static Evm::Instance<10,10,10> evm;
+static PinInputAnalog pia1(PIN_ANALOG_1);
+static PinInputAnalog pia2(PIN_ANALOG_2);
+
+
+static void OnAnalogRead(uint8_t pin, uint16_t val)
+{
+    Serial.println(PAL.Millis());
+    
+    Serial.print(pin);
+    Serial.print(": ");
+    Serial.println(val);
+
+    Serial.println("");
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    pia1.SetCallback([=](uint16_t val) { OnAnalogRead(PIN_ANALOG_1, val); });
+    pia1.Enable();
+    pia2.SetCallback([=](uint16_t val) { OnAnalogRead(PIN_ANALOG_2, val); });
+    pia2.Enable();
+
+    evm.MainLoop();
+}
+
+void loop() {}
+
+
+
