fa743115651cbb4a92742130fd5770028c381f21
dmalnati <douglas.malnati@gmail.com>
2016-12-31 00:04:20 -0500

    AVR Keypad16Button initial check-in

---
+++ sketches/TestKeypad16Button/TestKeypad16Button.ino
@@ -0,0 +1,31 @@
+#include "Evm.h"
+#include "Keypad16Button.h"
+
+
+static Evm::Instance<10,10,10> evm;
+
+static Keypad16Button kpad({
+    11, 12, 13, 14, 15, 16, 17, 18
+});
+
+
+void setup()
+{
+    Serial.begin(9600);
+
+    Serial.println("Starting");
+    
+    kpad.Init([](char c) {
+        Serial.println(c);
+    });
+
+    evm.MainLoop();
+}
+
+void loop() {}
+
+
+
+
+
+
