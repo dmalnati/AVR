77f29c7b07e7f36f7947cd4c887fdad4cf0d7bab
dmalnati <douglas.malnati@gmail.com>
2017-03-14 23:06:40 -0400

    AVR CRC header touchup

--- libraries/Utl/crc.h
+++ libraries/Utl/crc.h
@@ -2,25 +2,10 @@
 #define __CRC_H__
 
 
-// Code from:
-// http://www.evilgeniuslair.com/2015/01/14/crc-8/
-//CRC-8 - based on the CRC8 formulas by Dallas/Maxim
-//code released under the therms of the GNU GPL 3.0 license
-byte CRC8(const byte *data, byte len) {
-    byte crc = 0x00;
-    while (len--) {
-        byte extract = *data++;
-        for (byte tempI = 8; tempI; tempI--) {
-            byte sum = (crc ^ extract) & 0x01;
-            crc >>= 1;
-            if (sum) {
-                crc ^= 0x8C;
-            }
-            extract >>= 1;
-        }
-    }
-    return crc;
-}
+#include <stdint.h>
+
+
+extern uint8_t CRC8(const uint8_t *data, uint8_t len);
 
 
 #endif  // __CRC_H__
