9eb82b0ba265c681fdb1db7c65f654eb21c3d794
dmalnati <douglas.malnati@gmail.com>
2017-04-10 19:00:02 -0400

    AVR SensorSoilMoisture mid-development checkin

---
+++ libraries/SensorSoilMoisture/SensorSoilMoisture.h
@@ -0,0 +1,79 @@
+#ifndef __SENSOR_SOIL_MOISTURE_H__
+#define __SENSOR_SOIL_MOISTURE_H__
+
+
+#include <stdint.h>
+
+#include "PAL.h"
+
+
+class SensorSoilMoisture
+{
+public:
+    SensorSoilMoisture(uint8_t pinSensorPower, uint8_t pinSensorMeasurement)
+    : pinSensorPower_(pinSensorPower)
+    , pinSensorMeasurement_(pinSensorMeasurement)
+    {
+        PAL.PinMode(pinSensorPower_, OUTPUT);
+        PAL.DigitalWrite(pinSensorPower_, LOW);
+        
+        PAL.PinMode(pinSensorMeasurement_, INPUT);
+    }
+    
+    ~SensorSoilMoisture()
+    {
+        // Nothing to do
+    }
+    
+    struct Measurement
+    {
+        uint8_t pctConductive;
+    };
+    
+    uint8_t GetMeasurement(Measurement *m)
+    {
+        uint8_t retVal = 0;
+        
+        if (m)
+        {
+            retVal = 1;
+            
+            // Enable power
+            PAL.DigitalWrite(pinSensorPower_, HIGH);
+            
+            // Wait(?)
+            PAL.Delay(50);
+            
+            // Read analog value
+            uint16_t adcVal = PAL.AnalogRead(pinSensorMeasurement_);
+            
+            // Disable power
+            PAL.DigitalWrite(pinSensorPower_, LOW);
+            
+            
+            static const uint16_t MAX_ADC_VALUE = 1024;
+            
+            
+            Serial.print("adcVal: ");
+            Serial.println(adcVal);
+            Serial.print("MAX_ADC_VALUE: ");
+            Serial.println(MAX_ADC_VALUE);
+            double pctConductiveFloat = ((double)adcVal / MAX_ADC_VALUE * 100.0);
+            Serial.print("pctConductiveFloat: ");
+            Serial.println(pctConductiveFloat);
+            
+            // Convert to percentage
+            m->pctConductive = (uint8_t)((double)adcVal / MAX_ADC_VALUE * 100.0);
+        }
+        
+        return retVal;
+    }
+
+private:
+
+    uint8_t pinSensorPower_;
+    uint8_t pinSensorMeasurement_;
+};
+
+
+#endif  // __SENSOR_SOIL_MOISTURE_H__
---
+++ sketches/TestSensorSoilMoisture/TestSensorSoilMoisture.ino
@@ -0,0 +1,43 @@
+#include "PAL.h"
+#include "SensorSoilMoisture.h"
+
+
+static const uint8_t PIN_POWER       = 16;
+static const uint8_t PIN_MEASUREMENT = 25;
+
+//static SensorSoilMoisture s(PIN_POWER, PIN_MEASUREMENT);
+
+
+void setup()
+{
+    Serial.begin(9600);
+
+    SensorSoilMoisture s(PIN_POWER, PIN_MEASUREMENT);
+    SensorSoilMoisture::Measurement m;
+
+    while (1)
+    {
+        if (s.GetMeasurement(&m))
+        {
+            Serial.println("Measurement successful");
+            
+            Serial.print("pctConductive: ");
+            Serial.println(m.pctConductive);
+        }
+        else
+        {
+            Serial.println("Measurement failure");
+        }
+    
+        Serial.println();
+    
+        PAL.Delay(1000);
+
+    }
+}
+
+void loop() {}
+
+
+
+
