96e9c14a6fd59df378b6427252f1be899791058c
dmalnati <douglas.malnati@gmail.com>
2017-07-05 20:51:16 -0400

    AVR TestSignalSourceSineWave mid-development checkin

--- sketches/TestSignalSourceSineWave/TestSignalSourceSineWave.ino
+++ sketches/TestSignalSourceSineWave/TestSignalSourceSineWave.ino
@@ -6,26 +6,13 @@ static SignalSourceSineWave ss;
 static SignalSourceSineWave::IdxType idx;
 
 
-void setup()
-{
-    Serial.begin(9600);
-
-    while (1)
-    {
-        TestSignalSourceSineWave(8000, 1200);
-        TestSignalSourceSineWave(44100, 1200);
-
-        PAL.Delay(1000);
-    }
-}
-
 void TestSignalSourceSineWave(uint16_t sampleRate, uint16_t frequency)
 {
     Serial.println();
     Serial.print("sampleRate: "); Serial.println(sampleRate);
     Serial.print("frequency: "); Serial.println(frequency);
 
-    //idx.ResetIdx();
+    //idx.Reset();
     idx.Calibrate(sampleRate, frequency);
 
     for (uint16_t i = 0; i < 30; ++i)
@@ -36,6 +23,85 @@ void TestSignalSourceSineWave(uint16_t sampleRate, uint16_t frequency)
         Serial.print(", val: "); Serial.println(val);
 
         ++idx;
+    }
+}
+
+void TestSignalSourceSineWaveRange(uint16_t sampleRate,
+                                   uint16_t freqLow,
+                                   uint16_t freqHigh,
+                                   uint16_t freqStep)
+{
+    for (uint16_t freq = freqLow; freq <= freqHigh; freq += freqStep)
+    {
+        Serial.println();
+        Serial.print("sampleRate: "); Serial.println(sampleRate);
+        Serial.print("frequency: "); Serial.println(freq);
+        
+        idx.Reset();
+        idx.Calibrate(sampleRate, freq);
+
+        uint8_t cont = 1;
+
+        int16_t idxIntLast = -1;
+        int16_t valLast = -1;
+        while (cont)
+        {
+            uint16_t idxInt = (uint16_t)idx;
+            uint16_t val    = ss.GetSampleAtIdx(idx);
+
+            if (idxIntLast == -1) { idxIntLast = (int16_t)idxInt; }
+            if (valLast == -1)    { valLast = (int16_t)val;    }
+
+            uint8_t show = (idxIntLast != (int16_t)idxInt || valLast != (int16_t)val);
+
+            if (show)
+            {
+                Serial.print("  idxInt: "); Serial.print(idxInt);
+                Serial.print(", val: "); Serial.print(val);
+            }
+
+            // check for wrap so we can move along to next freq
+            if ((int16_t)idxInt < idxIntLast)
+            {
+                cont = 0;
+
+                if (show) { Serial.print("   (breakout)"); }
+            }
+
+            if (show) { Serial.println(); }
+
+            idxIntLast = idxInt;
+            valLast = val;
+
+            ++idx;
+        }
+    }
+}
+
+void TestProblemFrequencies()
+{
+    idx.Reset();
+    TestSignalSourceSineWave(40000, 506);
+    PAL.Delay(5000);
+
+    idx.Reset();
+    TestSignalSourceSineWave(40000, 515);
+    PAL.Delay(5000);
+}
+
+
+void setup()
+{
+    Serial.begin(9600);
+
+    while (1)
+    {
+        //TestSignalSourceSineWave(8000, 1200);
+        //TestSignalSourceSineWave(44100, 1200);
+        //TestSignalSourceSineWaveRange(40000, 1, 100, 1);
+        TestProblemFrequencies();
+
+        PAL.Delay(1000);
     }
 }
 
