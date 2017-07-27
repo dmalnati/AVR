8198b04a14dabf8bfaa48928160ed59c9fa9775d
dmalnati <douglas.malnati@gmail.com>
2017-07-27 01:26:36 -0400

    AVR PianoKeyboardPimoroniHAT

--- libraries/HID/PianoKeyboardPimoroniHAT.h
+++ libraries/HID/PianoKeyboardPimoroniHAT.h
@@ -2,7 +2,6 @@
 #define __PIANO_KEYBOARD_PIMORONI_HAT_H__
 
 
-#include "TWI.h"
 #include "Function.h"
 #include "TimedEventHandler.h"
 #include "SensorCapacitiveTouchCAP1188.h"
@@ -19,6 +18,11 @@ public:
 
     PianoKeyboardPimoroniHAT()
     : cap1_(CAP1_ADDR)
+    , cap2_(CAP2_ADDR)
+    , touchedBitmapLast_(0)
+    , isTouched_(0)
+    , touchedKeyNumber_(0)
+    , ignoreKeyCNextOctave_(1)
     {
         // Nothing to do
     }
@@ -29,6 +33,23 @@ public:
             OnPoll();
         });
         ted_.RegisterForTimedEventInterval(DEFAULT_POLL_PERIOD);
+    }
+    
+    void EnableLEDs()
+    {
+        cap1_.EnableLEDs();
+        cap2_.EnableLEDs();
+    }
+    
+    void DisableLEDs()
+    {
+        cap1_.DisableLEDs();
+        cap2_.DisableLEDs();
+    }
+    
+    void SetIgnoreKeyCNextOctave(uint8_t ignoreKeyCNextOctave)
+    {
+        ignoreKeyCNextOctave_ = ignoreKeyCNextOctave;
     }
 
     void SetCallbackOnKeyDown(function<void(uint8_t)> cbFnOnKeyDown)
@@ -61,10 +82,141 @@ private:
 
     void OnPoll()
     {
+        // Get current state of touched keys
+        uint8_t cap1Touched = cap1_.GetTouched();
+        uint8_t cap2Touched = cap2_.GetTouched();
         
+        uint16_t touchedBitmap = (cap2Touched << 8) | cap1Touched;
+        
+        // Examine bitmap for key changes
+        uint8_t checkForTouchedKey = 0;
+        
+        if (isTouched_)
+        {
+            // Was being touched as of last time
+            
+            // Check if prior key still being touched, if yes, do nothing
+            if (touchedBitmap & (1 << touchedKeyNumber_))
+            {
+                // Do nothing
+            }
+            else
+            {
+                // Prior key is now up, fire callback if not a special key
+                if (touchedKeyNumber_ <= 12)
+                {
+                    cbFnOnKeyUp_(touchedKeyNumber_);
+                }
+                
+                // Reset state to show no key is pressed
+                isTouched_        = 0;
+                touchedKeyNumber_ = 0;
+                
+                // Search for currently touched key
+                checkForTouchedKey = 1;
+            }
+        }
+        else
+        {
+            // Not touched as of last time
+            
+            // Search for currently touched key
+            checkForTouchedKey = 1;
+        }
+        
+        if (checkForTouchedKey)
+        {
+            // Find the first touched key, if present, and fire callback
+            uint8_t cont = 1;
+            for (int8_t i = 15; i >= 0 && cont; --i)
+            {
+                uint8_t isTouched = !!(touchedBitmap & (1 << i));
+                
+                if (isTouched)
+                {
+                    uint8_t trackTouch = 1;
+                    
+                    // First key found, fire callback, depending on type
+                    if (i <= 12)
+                    { 
+                        if (ignoreKeyCNextOctave_ && i == 12)
+                        {
+                            // Do nothing
+                            trackTouch = 0;
+                        }
+                        else if (ignoreKeyCNextOctave_ && i != 12)
+                        {
+                            cbFnOnKeyDown_(i);
+                        }
+                    }
+                    else if (i == 13)
+                    {
+                        cbFnOnOctaveKeyDownPress_();
+                    }
+                    else if (i == 14)
+                    {
+                        cbFnOnOctaveKeyUpPress_();
+                    }
+                    else if (i == 15)
+                    {
+                        cbFnOnInstrumentChangeKeyPress_();
+                    }
+                    
+                    if (trackTouch)
+                    {
+                        // Save state for next time
+                        isTouched_        = 1;
+                        touchedKeyNumber_ = i;
+                    }
+                    
+                    // Break out of search
+                    cont = 0;
+                }
+            }
+        }
+        
+        // Save state for next time
+        touchedBitmapLast_ = touchedBitmap;
     }
     
+    /*
+     * cap1 76543210
+     * - 7 = C
+     * - 6 = C_SHARP
+     * - 5 = D
+     * - 4 = D_SHARP
+     * - 3 = E
+     * - 2 = F
+     * - 1 = F_SHARP
+     * - 0 = G
+     *
+     * cap2 76543210
+     * - 7 = G_SHARP
+     * - 6 = A
+     * - 5 = A_SHARP
+     * - 4 = B
+     * - 3 = C (next octave)
+     * - 2 = octave down
+     * - 1 = octave up
+     * - 0 = next instrument
+     *
+     * But the key number scheme is treated like a zero-offset array of 16 vals
+     *      0000000000111111
+     *      0123456789012345
+     * cap1 76543210
+     * cap2         76543210
+     *
+     */
+    
     SensorCapacitiveTouchCAP1188 cap1_;
+    SensorCapacitiveTouchCAP1188 cap2_;
+    
+    uint16_t touchedBitmapLast_;
+    
+    uint8_t isTouched_;
+    uint8_t touchedKeyNumber_;
+    
+    uint8_t ignoreKeyCNextOctave_;
     
     function<void(uint8_t)>  cbFnOnKeyDown_;
     function<void(uint8_t)>  cbFnOnKeyUp_;
--- sketches/TestPianoKeyboardPimoroniHAT/TestPianoKeyboardPimoroniHAT.ino
+++ sketches/TestPianoKeyboardPimoroniHAT/TestPianoKeyboardPimoroniHAT.ino
@@ -1,9 +1,37 @@
+#include "Evm.h"
+#include "Function.h"
 #include "PianoKeyboardPimoroniHAT.h"
+
+Evm::Instance<10,10,10> evm;
+PianoKeyboardPimoroniHAT hat;
 
 
 void setup()
 {
+    Serial.begin(9600);
 
+    Serial.println("Starting");
+    
+    hat.SetCallbackOnKeyDown([](uint8_t key){
+        Serial.print("OnKeyDown: "); Serial.println(key);
+    });
+    hat.SetCallbackOnKeyUp([](uint8_t key){
+        Serial.print("OnKeyUp  : "); Serial.println(key);
+    });
+    hat.SetCallbackOnInstrumentChangeKeyPress([](){
+        Serial.println("OnInstrumentChangeKeyPress");
+    });
+    hat.SetCallbackOnOctaveKeyUpPress([](){
+        Serial.println("OnOctaveKeyUpPress");
+    });
+    hat.SetCallbackOnOctaveKeyDownPress([](){
+        Serial.println("OnOctaveKeyDownPress");
+    });
+
+    hat.EnableLEDs();
+    hat.Init();
+
+    evm.MainLoop();
 }
 
 void loop() {}
