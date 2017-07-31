6d8dea8dfb01fde9c1400add3dc981688fbb4be4
dmalnati <douglas.malnati@gmail.com>
2017-07-30 22:34:51 -0400

    AVR ShiftRegisterInput complete

--- libraries/Peripheral/ShiftRegisterInput.h
+++ libraries/Peripheral/ShiftRegisterInput.h
@@ -19,7 +19,6 @@ class ShiftRegisterInput
     struct Callback
     {
         uint8_t mode;
-        uint8_t activeLevel;
         CbFn    cbFn;
     };
     
@@ -36,24 +35,23 @@ public:
         
         for (uint16_t i = 0; i < CB_FN_LIST_LEN; ++i)
         {
-            SetCallback(i, cbFnDefault, LEVEL_RISING_AND_FALLING, HIGH);
+            SetCallback(i, cbFnDefault, LEVEL_RISING_AND_FALLING);
         }
     }
     
     void SetCallback(uint8_t                             pinLogical,
                      function<void(uint8_t logicLevel)>  cbFn,
-                     uint8_t                             mode        = LEVEL_RISING,
-                     uint8_t                             activeLevel = LOW)
+                     uint8_t                             mode = LEVEL_RISING)
     {
-        uint8_t cbListIdx = ((pinLogical / 8) * 8) + (pinLogical % 8);
-        
         Callback c;
         
-        c.cbFn        = cbFn;
-        c.mode        = mode;
-        c.activeLevel = activeLevel;
+        c.cbFn = cbFn;
+        c.mode = mode;
         
-        cbList_[cbListIdx] = c;
+        if (pinLogical < CB_FN_LIST_LEN)
+        {
+            cbList_[pinLogical] = c;
+        }
     }
     
     void Init(uint8_t forceRefresh = 0)
@@ -87,16 +85,16 @@ private:
             uint8_t bitmapLast = bufLast[i];
             uint8_t bitmap     = buf[i];
 
-            if (bitmap != bitmapLast)
+            if (bitmap != bitmapLast || forceRefresh)
             {
-                for (uint8_t j = 0; j < 8; ++j)
+                for (int8_t j = 7; j >= 0; --j)
                 {
                     uint8_t bitVal     = !!(bitmap     & (1 << (7 - j)));
                     uint8_t bitValLast = !!(bitmapLast & (1 << (7 - j)));
                     
                     if (bitVal != bitValLast || forceRefresh)
                     {
-                        uint8_t cbListIdx = (i * 8) + j;
+                        uint8_t cbListIdx = (i * 8) + (7 - j);
                         
                         OnPinChange(cbListIdx, bitVal);
                     }
@@ -112,15 +110,14 @@ private:
         Callback c = cbList_[cbListIdx];
         
         uint8_t invokeCallback = 0;
-        uint8_t bitValLogical  = c.activeLevel == HIGH ? bitVal : !bitVal;
         
         if (c.mode == LEVEL_UNDEFINED || c.mode == LEVEL_RISING)
         {
-            invokeCallback = bitValLogical;
+            invokeCallback = bitVal;
         }
         else if (c.mode == LEVEL_FALLING)
         {
-            invokeCallback = !bitValLogical;
+            invokeCallback = !bitVal;
         }
         else
         {
@@ -129,7 +126,7 @@ private:
         
         if (invokeCallback)
         {
-            c.cbFn(bitValLogical);
+            c.cbFn(bitVal);
         }
     }
     
---
+++ sketches/TestShiftRegisterInput/TestShiftRegisterInput.ino
@@ -0,0 +1,76 @@
+#include "Evm.h"
+#include "ShiftRegister.h"
+#include "ShiftRegisterInput.h"
+
+
+static const uint8_t PIN_LOAD         = 14;
+static const uint8_t PIN_CLOCK        = 13;
+static const uint8_t PIN_CLOCK_ENABLE = 12;
+static const uint8_t PIN_SERIAL       = 11;
+
+
+static Evm::Instance<10,10,10> evm;
+
+static ShiftRegister sr(PIN_LOAD, PIN_CLOCK, PIN_CLOCK_ENABLE, PIN_SERIAL);
+static ShiftRegisterInput<2> sri(sr, 20, [](uint8_t logicLevel){
+    Serial.print("default logicLevel change: ");
+    Serial.print(logicLevel);
+    Serial.println();
+});
+
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting");
+
+    Monitor(2);
+
+    uint8_t forceRefresh = 1;
+    sri.Init(forceRefresh);
+
+    evm.MainLoop();
+}
+
+void Monitor(uint8_t srCount)
+{
+    uint8_t modeList[] = { LEVEL_RISING, LEVEL_FALLING, LEVEL_RISING_AND_FALLING };
+    
+    for (uint8_t i = 0; i < (srCount * 8) - 1; ++i)
+    {
+        Serial.print("Setting up pin ");
+        Serial.print(i);
+        Serial.print(", mode: ");
+        Serial.print((i % 3) == 0 ? "LEVEL_RISING" : (i % 3) == 1 ? "LEVEL_FALLING" : "LEVEL_RISING_AND_FALLING");
+        Serial.println();
+        
+        sri.SetCallback(i, [=](uint8_t logicLevel){
+            Serial.print("Pin ");
+            Serial.print(i);
+            Serial.print(": ");
+            Serial.print(logicLevel);
+            Serial.println();
+        }, modeList[i % 3] );
+    }
+}
+
+
+void loop() {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
