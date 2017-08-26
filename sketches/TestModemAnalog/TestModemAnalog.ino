1e001b16a4d9b07bffcb0b9a23e8dbef3db88751
dmalnati <douglas.malnati@gmail.com>
2017-08-26 18:18:13 -0400

    AVR Modem re-work

---
+++ sketches/TestModemAnalog/TestModemAnalog.ino
@@ -0,0 +1,36 @@
+#include "PAL.h"
+#include "ModemAnalog.h"
+
+
+static const uint16_t SAMPLE_RATE = 40000;
+static const uint16_t FREQ1 = 1200;
+static const uint16_t FREQ2 = 2200;
+
+static ModemAnalog ma;
+
+
+void setup()
+{
+    uint16_t freqList[] = {
+        FREQ1,
+        FREQ2,
+    };
+    uint8_t idx = 0;
+    
+    ma.SetSampleRate(SAMPLE_RATE);
+
+    ma.Start();
+    
+    while (1)
+    {
+        ma.SetFrequency(freqList[idx]);
+        idx = !idx;
+        
+        PAL.Delay(500);
+    }
+}
+
+void loop() {}
+
+
+
---
+++ sketches/TestModemBell202/TestModemBell202.ino
@@ -0,0 +1,72 @@
+#include "PAL.h"
+#include "ModemBell202.h"
+
+
+static ModemBell202 m;
+
+
+
+void TestSendJustZeros()
+{
+    uint8_t buf[] = { 0b00000000 };
+
+    m.Start();
+
+    m.Send(buf, 1);
+    
+    m.Stop();
+}
+
+
+
+void TestSendJustOnes()
+{
+    uint8_t buf[] = { 0b11111111 };
+
+    m.Start();
+
+    m.Send(buf, 1);
+    
+    m.Stop();
+}
+
+void TestSendJustOnesNoStuff()
+{
+    uint8_t buf[] = { 0b11111111 };
+
+    m.Start();
+
+    m.Send(buf, 1, 0);
+    
+    m.Stop();
+}
+
+
+void setup()
+{
+    Serial.begin(9600);
+    
+    m.Init();
+
+    while (1)
+    {
+        TestSendJustZeros();
+        PAL.Delay(500);
+        
+        TestSendJustOnes();
+        PAL.Delay(500);
+
+        TestSendJustOnesNoStuff();
+        PAL.Delay(500);
+    }
+}
+
+
+
+
+
+
+void loop() {}
+
+
+
