59b3e1e928d54beb75cf0cb2b6e42f9600b80246
dmalnati <douglas.malnati@gmail.com>
2017-09-13 01:23:07 -0400

    AVR add missed APRS Transmission Demo

---
+++ sketches/TestAPRSTransmissionDemo/TestAPRSTransmissionDemo.ino
@@ -0,0 +1,119 @@
+#include "PAL.h"
+#include "UtlStreamBlob.h"
+#include "AX25UIMessage.h"
+#include "ModemBell202.h"
+
+
+// A program that works, but doesn't use my callsign, so
+// I can post concrete examples on the internet without worrying that
+// other people will spam the airwaves with my info while debugging
+// their systems.
+
+
+static const uint8_t BUF_SIZE = 60;
+static uint8_t bufShared[BUF_SIZE] = { 0 };
+
+static AX25UIMessage msg;
+static ModemBell202 *modem;
+
+
+void Send(uint8_t *buf, uint8_t bufLen)
+{
+    // APRSDroid sends ~256 flags to start...
+    uint8_t flagList[]  = { 0x7E, 0x7E, 0x7E, 0x7E, 0x7E, 0x7E, 0x7E, 0x7E };
+    uint8_t flagListLen = sizeof(flagList);
+
+    uint8_t bitStuff = 0;
+
+    modem->Start();
+
+    // Send preamble, which also will serve as the flag byte
+    // Send lots, like APRSDroid
+    for (uint8_t i = 0; i < 6; ++i)
+    {
+        bitStuff = 0;
+        modem->Send(flagList, flagListLen, bitStuff);
+    }
+
+    // send content
+    bitStuff = 1;
+    modem->Send(buf, bufLen, bitStuff);
+
+    // Send trailing flags
+    bitStuff = 0;
+    modem->Send(flagList, 3, bitStuff);
+
+    modem->Stop();
+}
+
+void DoMessageTest()
+{
+    uint8_t *buf     = bufShared;
+    uint8_t  bufSize = BUF_SIZE;
+    
+    msg.Init(buf);
+
+    msg.SetDstAddress("DST", 2);
+    msg.SetSrcAddress("SRC", 3);
+    msg.AddRepeaterAddress("WIDE1", 1);
+
+    Serial.println("Post SetAddress");
+    StreamBlob(Serial, buf, bufSize, 1);
+
+    const char *info = "Just Some Data";
+    uint8_t infoLen = strlen(info);   // 16
+    msg.AppendInfo((uint8_t *)info, infoLen);
+
+    Serial.println("Post AppendInfo");
+    StreamBlob(Serial, buf, bufSize, 1);
+    
+    uint8_t bytesUsed = msg.Finalize();
+    Serial.print("Post Finalize (");
+    Serial.print(bytesUsed);
+    Serial.print(" bytes used)");
+    Serial.println();
+
+    Serial.println("Container buffer");
+    StreamBlob(Serial, buf, bufSize, 1);
+
+    Serial.println("Completed buffer (just the used parts)");
+    StreamBlob(Serial, buf, bytesUsed, 1);
+
+    Serial.println();
+    Serial.println();
+
+    Send(buf, bytesUsed);
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    // So I can use Serial in the class for debugging...
+    ModemBell202 modemReal;
+    modem = &modemReal;
+
+    modem->Init();
+
+    while (1)
+    {
+        DoMessageTest();
+        PAL.Delay(1000);
+    }
+}
+
+
+
+void loop() { }
+
+
+
+
+
+
+
+
+
+
+
+
