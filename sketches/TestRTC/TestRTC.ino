533d897031adb287e1090d817818fdda22cdc303
dmalnati <douglas.malnati@gmail.com>
2017-10-30 18:29:19 -0400

    AVR RTC initial checkin

---
+++ sketches/TestRTC/TestRTC.ino
@@ -0,0 +1,24 @@
+#include "RTC.h"
+
+
+static RTC rtc;
+
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting");
+
+    rtc.Sync(23, 59, 50, 0);
+
+    while (1)
+    {
+        Serial.println(rtc.GetTimeAsString());
+
+        PAL.Delay(500);
+    }
+}
+
+void loop() {}
+
+
