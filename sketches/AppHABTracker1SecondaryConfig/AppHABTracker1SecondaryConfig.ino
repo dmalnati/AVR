db6d0b7b5173bd793eddc86cdebc3f1896a90ece
dmalnati <douglas.malnati@gmail.com>
2017-11-19 20:51:08 -0500

    AVR AppHABTracker1Secondary initial checkin

---
+++ sketches/AppHABTracker1SecondaryConfig/AppHABTracker1SecondaryConfig.ino
@@ -0,0 +1,21 @@
+#include "AppHABTracker1Secondary.h"
+
+
+static AppHABTracker1SecondaryConfig cfg =
+{
+    .pinRfTx = 14,
+    
+    .reportIntervalMs = 5000,
+};
+
+static AppHABTracker1Secondary app(cfg);
+
+
+void setup()
+{
+    app.Run();
+}
+
+void loop() {}
+
+
