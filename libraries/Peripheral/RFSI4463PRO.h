24b3776e5b498852e34df4207ae636341f7f2d08
dmalnati <douglas.malnati@gmail.com>
2017-12-10 16:36:43 -0500

    AVR RFSI4463PRO class initial check-in

---
+++ libraries/Peripheral/RFSI4463PRO.h
@@ -0,0 +1,219 @@
+#ifndef __RF_SI4463PRO_H__
+#define __RF_SI4463PRO_H__
+
+
+#include <SPI.h>
+
+#include "PAL.h"
+#include "BufferFieldExtractor.h"
+
+
+// Some notes regarding implementation were taken from here:
+// https://github.com/Yveaux/RadioHead/blob/master/RadioHead/RH_RF24.cpp
+//
+// Such as:
+// - Waiting briefly to let SS pin go high after transfer
+// - Using two transactions for command and reading of response
+// - Power-on-Reset functionality being necessary above tying Shutdown to GND
+//
+// These details are scattered in specs and emails as well.
+//
+
+
+class RFSI4463PRO
+{
+    // SPI values relating to pure SPI interfacing
+    static const uint32_t SPI_SPEED           = 1000000;
+    static const uint8_t  SPI_BIT_ORIENTATION = MSBFIRST;
+    static const uint8_t  SPI_MODE            = SPI_MODE0;
+    
+    // SPI values relating to particulars established for this chip
+    static const uint8_t READ_ATTEMPT_LIMIT                  = 100;
+    static const uint8_t DELAY_US_BEFORE_CHIP_SELECT_RELEASE = 50;
+    
+    // Details relating to operating this chip    
+    static const uint32_t DURATION_MS_POWER_ON_RESET = 50;
+    
+
+    // Commands
+    enum
+    {
+        CMD_PART_INFO = 0x01,
+        CMD_READ_CMD_BUFF = 0x44,
+    };
+    
+    // Various relevant values
+    enum
+    {
+        VAL_TO_SEND_WHEN_READING = 0xFF,
+        VAL_CLEAR_TO_SEND = 0xFF,
+    };
+
+
+
+public:
+    RFSI4463PRO(uint8_t pinChipSelect, uint8_t pinShutdown)
+    : pinChipSelect_(pinChipSelect)
+    , pinShutdown_(pinShutdown)
+    {
+        // Nothing to do
+    }
+    
+    void Init()
+    {
+        SPI.begin();
+        
+        PAL.PinMode(pinChipSelect_, OUTPUT);
+        PAL.DigitalWrite(pinChipSelect_, HIGH);
+        
+        PowerOnReset();
+    }
+    
+    struct PartInfo
+    {
+        uint8_t  chipRevision;
+        uint16_t partNumber;
+        uint8_t  partBuild;
+        uint16_t id;
+        uint8_t  customerId;
+        uint8_t  romId;
+    };
+    
+    void DumpBuffer(uint8_t *buf, uint8_t bufLen)
+    {
+        const char *sep = "";
+        for (uint8_t i = 0; i < bufLen; ++i)
+        {
+            Serial.print(sep);
+            Serial.print(buf[i], HEX);
+            
+            sep = " ";
+        }
+        Serial.println();
+    }
+    
+
+    
+    uint8_t GetPartInfo(PartInfo &partInfo)
+    {
+        const uint8_t BUF_SIZE = 8;
+        uint8_t       buf[BUF_SIZE];
+        
+        uint8_t retVal = SendAndWaitAndReceive(CMD_PART_INFO, buf, BUF_SIZE);
+        
+        if (retVal)
+        {
+            BufferFieldExtractor bfe(buf, BUF_SIZE);
+            
+            partInfo.chipRevision = bfe.GetUI8();
+            partInfo.partNumber   = bfe.GetUI16NTOHS();
+            partInfo.partBuild    = bfe.GetUI8();
+            partInfo.id           = bfe.GetUI16NTOHS();
+            partInfo.customerId   = bfe.GetUI8();
+            partInfo.romId        = bfe.GetUI8();
+        }
+        
+        return retVal;
+    }
+    
+    
+private:
+
+    void PowerOnReset()
+    {
+        PAL.PinMode(pinShutdown_, OUTPUT);
+
+        PAL.DigitalWrite(pinShutdown_, LOW);
+        PAL.Delay(DURATION_MS_POWER_ON_RESET);
+        
+        PAL.DigitalWrite(pinShutdown_, HIGH);
+        PAL.Delay(DURATION_MS_POWER_ON_RESET);
+        
+        PAL.DigitalWrite(pinShutdown_, LOW);
+        PAL.Delay(DURATION_MS_POWER_ON_RESET);
+    }
+    
+    
+    
+    
+    
+    
+    uint8_t SendAndWaitAndReceive(uint8_t req, uint8_t *repBuf, uint8_t repBufLen)
+    {
+        uint8_t retVal = 0;
+        
+        //
+        // Step 1 -- Issue command
+        //
+        SPI.beginTransaction(SPISettings(SPI_SPEED, SPI_BIT_ORIENTATION, SPI_MODE));
+        PAL.DigitalWrite(pinChipSelect_, LOW);
+
+        SPI.transfer(req);
+
+        PAL.DelayMicroseconds(DELAY_US_BEFORE_CHIP_SELECT_RELEASE);
+        
+        PAL.DigitalWrite(pinChipSelect_, HIGH);
+        SPI.endTransaction();
+        
+        //
+        // Step 2 -- Wait for chip to indicate that the result of the above
+        // command is complete and any data ready to read.
+        //
+        // This is done by issuing another command where:
+        // - issue the second command
+        // - wait for a CTS (clear to send) indication to be returned
+        //   (despite the fact that we're actually waiting to read, not send)
+        // - read however many bytes are expected
+        //
+        // If the chip indicates the results aren't ready to be read, start
+        // another complete transactional attempt to read again.
+        //
+        uint8_t readAttempts = 0;
+        uint8_t rep          = !VAL_CLEAR_TO_SEND;
+        while (rep != VAL_CLEAR_TO_SEND && readAttempts < READ_ATTEMPT_LIMIT)
+        {
+            ++readAttempts;
+            
+            SPI.beginTransaction(SPISettings(SPI_SPEED, SPI_BIT_ORIENTATION, SPI_MODE));
+            PAL.DigitalWrite(pinChipSelect_, LOW);
+
+            // Send command indicating we want to see the result of the previous
+            // command.
+            SPI.transfer(CMD_READ_CMD_BUFF);
+            
+            // Now wait for an indication that the result of the first command is
+            // actually ready to read.
+            rep = SPI.transfer(VAL_TO_SEND_WHEN_READING);
+
+            if (rep == VAL_CLEAR_TO_SEND)
+            {
+                retVal = 1;
+                
+                // Command result ready to read.
+                // Consume as many bytes of response as are expected
+                // (could be zero based on input, which is valid)
+                for (uint16_t i = 0; i < repBufLen; ++i)
+                {
+                    repBuf[i] = SPI.transfer(VAL_TO_SEND_WHEN_READING);
+                }
+            }
+
+            PAL.DelayMicroseconds(DELAY_US_BEFORE_CHIP_SELECT_RELEASE);
+            
+            PAL.DigitalWrite(pinChipSelect_, HIGH);
+            SPI.endTransaction();
+        }
+        
+        return retVal;
+    }
+
+private:
+
+    uint8_t pinChipSelect_;
+    uint8_t pinShutdown_;
+
+};
+
+
+
+#endif  // __RF_SI4463PRO_H__
---
+++ sketches/TestRFSI4463PRO2/TestRFSI4463PRO2.ino
@@ -0,0 +1,54 @@
+#include "RFSI4463PRO.h"
+
+static const uint8_t PIN_SS       = 15;
+static const uint8_t PIN_SHUTDOWN = 14;
+
+static RFSI4463PRO rf(PIN_SS, PIN_SHUTDOWN);
+
+
+void GetPartInfo()
+{
+    Serial.println("GetPartInfo");
+    
+    RFSI4463PRO::PartInfo partInfo;
+
+    if (rf.GetPartInfo(partInfo))
+    {
+        Serial.print("chipRevision: "); Serial.println(partInfo.chipRevision);
+        Serial.print("partNumber  : "); Serial.println(partInfo.partNumber);
+        Serial.print("partBuild   : "); Serial.println(partInfo.partBuild);
+        Serial.print("id          : "); Serial.println(partInfo.id);
+        Serial.print("customerId  : "); Serial.println(partInfo.customerId);
+        Serial.print("romId       : "); Serial.println(partInfo.romId);
+    }
+    else
+    {
+        Serial.println("ERR");
+    }
+
+    Serial.println();
+}
+
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting");
+
+    rf.Init();
+
+    while (1)
+    {
+        GetPartInfo();
+
+
+        PAL.Delay(5000);
+    }
+}
+
+
+void loop() {}
+
+
+
+
