22050ab6feb848c8f8fa6d76ac822a157e6fbde1
dmalnati <douglas.malnati@gmail.com>
2018-02-03 16:42:40 -0500

    AVR AppPicoTracker1 UserConfig interface updates

--- libraries/AppPicoTracker1/AppPicoTracker1.h
+++ libraries/AppPicoTracker1/AppPicoTracker1.h
@@ -3,6 +3,7 @@
 
 
 #include "Evm.h"
+#include "AppPicoTracker1UserConfigSerialInterface.h"
 
 
 
@@ -10,57 +11,6 @@ struct AppPicoTracker1Config
 {
 };
 
-struct UserConfig
-{
-public:
-    static const uint8_t DEVICE_ID_LEN     = 20;
-    static const uint8_t CALLSIGN_LEN      = 6;
-    static const uint8_t NOTES_MESSAGE_LEN = 10;
-    
-public:
-    UserConfig()
-    {
-        memset((void *)deviceId,     '\0', DEVICE_ID_LEN     + 1);
-        memset((void *)callsign,     '\0', CALLSIGN_LEN      + 1);
-        memset((void *)notesMessage, '\0', NOTES_MESSAGE_LEN + 1);
-    }
-    
-    char *GetDeviceId() { return deviceId; }
-    void SetDeviceId(const char *val) { strncpy(deviceId, val, DEVICE_ID_LEN + 1); deviceId[DEVICE_ID_LEN] = '\0'; }
-    
-    char *GetCallsign() { return callsign; }
-    void SetCallsign(const char *val) { strncpy(callsign, val, CALLSIGN_LEN + 1); callsign[CALLSIGN_LEN] = '\0'; }
-    
-    uint8_t GetSsid() { return ssid; }
-    void SetSsid(uint8_t val) { ssid = val < 16 ? val : 15; }
-    
-    uint32_t GetReportIntervalSecs() { return reportIntervalSecs; }
-    void SetReportIntervalSecs(uint32_t val) { reportIntervalSecs = val; }
-    
-    uint8_t GetTransmitCount() { return transmitCount; }
-    void SetTransmitCount(uint8_t val) { transmitCount = val; }
-    
-    uint32_t GetDelaySecsBetweenTransmits() { return delaySecsBetweenTransmits; }
-    void SetDelaySecsBetweenTransmits(uint32_t val) { delaySecsBetweenTransmits = val; }
-
-    char *GetNotesMessage() { return notesMessage; }
-    void SetNotesMessage(const char *val) { strncpy(notesMessage, val, NOTES_MESSAGE_LEN + 1); notesMessage[NOTES_MESSAGE_LEN] = '\0'; }
-    
-private:
-    char deviceId[DEVICE_ID_LEN + 1];
-    
-    char    callsign[CALLSIGN_LEN + 1];
-    uint8_t ssid = 9;
-    
-    uint32_t reportIntervalSecs = 60;
-    
-    uint8_t  transmitCount             = 2;
-    uint32_t delaySecsBetweenTransmits = 3;
-    
-    char notesMessage[NOTES_MESSAGE_LEN + 1];
-};
-
-#include "AppPicoTracker1UserConfigSerialInterface.h"
 
 class AppPicoTracker1
 {
@@ -84,24 +34,37 @@ public:
         Serial.begin(9600);
         Serial.println(F("Starting"));
         
-        PAL.Delay(20);
-        
-        if (PAL.DigitalRead(2))
+        if (AppPicoTracker1UserConfigManager::GetUserConfig(userConfig_))
         {
-            Serial.println(F("Config mode enabled"));
+            Serial.println();
+            Serial.println(F("Proceeding with:"));
+            userConfig_.Print();
+            Serial.println();
             
-            AppPicoTracker1UserConfigSerialInterface menu;
             
-            menu.GetUserConfig();
+            
+            
+            
+            Serial.println(F("Running."));
+            Serial.println();
+            PAL.Delay(1000);
+            
+            evm_.MainLoopLowPower();
         }
         else
         {
-            Serial.println(F("Config mode not in use"));
+            Serial.println();
+            Serial.println(F("ERR: Invalid configuration, please restart and try again."));
+            Serial.println();
+            
+            // blink error -- cannot continue without config
         }
-        
-        while (1) {}
-        
-        // blink error if no valid configuration found
+    }
+
+    
+    
+    void ToDo()
+    {
         
         
         
@@ -111,13 +74,45 @@ public:
             
         // GPS geofencing to change frequency
     }
-
+    
+    
+    
 private:
 
     Evm::Instance<C_IDLE, C_TIMED, C_INTER> evm_;
 
     AppPicoTracker1Config &cfg_;
+    
+    AppPicoTracker1UserConfigManager::UserConfig userConfig_;
 };
 
 
 #endif  // __APP_PICO_TRACKER_1_H__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- libraries/AppPicoTracker1/AppPicoTracker1UserConfigSerialInterface.h
+++ libraries/AppPicoTracker1/AppPicoTracker1UserConfigSerialInterface.h
@@ -1,5 +1,5 @@
-#ifndef __APP_PICO_TRACKER_1_USER_CONFIG_SERIAL_INTERFACE_H__
-#define __APP_PICO_TRACKER_1_USER_CONFIG_SERIAL_INTERFACE_H__
+#ifndef __APP_PICO_TRACKER_1_USER_CONFIG_MANAGER_H__
+#define __APP_PICO_TRACKER_1_USER_CONFIG_MANAGER_H__
 
 
 #include "Eeprom.h"
@@ -7,23 +7,230 @@
 #include "UtlSerial.h"
 
 
-class AppPicoTracker1UserConfigSerialInterface
+class AppPicoTracker1UserConfigManager
 {
 public:
-
-    void GetUserConfig()
+    struct UserConfig
     {
-        EepromAccessor<UserConfig> ea;
-        ea.Read(userConfig_);
+    public:
+        static const uint8_t DEVICE_ID_LEN     = 20;
+        static const uint8_t CALLSIGN_LEN      = 6;
+        static const uint8_t NOTES_MESSAGE_LEN = 10;
         
-        PrintHelp();
-        
-        const uint8_t BUF_SIZE = 100;
-        char buf[BUF_SIZE];
-        
-        
-        while (1)
+    public:
+        UserConfig()
         {
+            memset((void *)deviceId,     '\0', DEVICE_ID_LEN     + 1);
+            memset((void *)callsign,     '\0', CALLSIGN_LEN      + 1);
+            memset((void *)notesMessage, '\0', NOTES_MESSAGE_LEN + 1);
+        }
+        
+        char *GetDeviceId() { return deviceId; }
+        void SetDeviceId(const char *val) { strncpy(deviceId, val, DEVICE_ID_LEN + 1); deviceId[DEVICE_ID_LEN] = '\0'; }
+        
+        char *GetCallsign() { return callsign; }
+        void SetCallsign(const char *val) { strncpy(callsign, val, CALLSIGN_LEN + 1); callsign[CALLSIGN_LEN] = '\0'; }
+        
+        uint32_t GetReportIntervalSecs() { return reportIntervalSecs; }
+        void SetReportIntervalSecs(uint32_t val) { reportIntervalSecs = val; }
+        
+        uint8_t GetTransmitCount() { return transmitCount; }
+        void SetTransmitCount(uint8_t val) { transmitCount = val; }
+        
+        uint32_t GetDelaySecsBetweenTransmits() { return delaySecsBetweenTransmits; }
+        void SetDelaySecsBetweenTransmits(uint32_t val) { delaySecsBetweenTransmits = val; }
+
+        char *GetNotesMessage() { return notesMessage; }
+        void SetNotesMessage(const char *val) { strncpy(notesMessage, val, NOTES_MESSAGE_LEN + 1); notesMessage[NOTES_MESSAGE_LEN] = '\0'; }
+        
+        void Print()
+        {
+            Serial.println(F("Current Configuration"));
+            Serial.println(F("---------------------"));
+            
+            Serial.println(F("Device Unique Identification"));
+            
+            Serial.print(F("    deviceId["));
+            Serial.print(DEVICE_ID_LEN);
+            Serial.print(F("]: "));
+            Serial.print(GetDeviceId());
+            Serial.println();
+            Serial.println();
+            
+            
+            Serial.println(F("Your Identity"));
+            
+            Serial.print(F("    callsign["));
+            Serial.print(CALLSIGN_LEN);
+            Serial.print(F("]: "));
+            Serial.print(GetCallsign());
+            Serial.println();
+            Serial.println();
+            
+            
+            Serial.println(F("Reporting Interval"));
+            
+            Serial.print(F("    reportIntervalSecs: "));
+            Serial.print(GetReportIntervalSecs());
+            Serial.println();
+            Serial.println();
+            
+            
+            Serial.println(F("Reliability of Transmission"));
+            
+            Serial.print(F("    transmitCount: "));
+            Serial.print(GetTransmitCount());
+            Serial.println();
+            
+            Serial.print(F("    delaySecsBetweenTransmits: "));
+            Serial.print(GetDelaySecsBetweenTransmits());
+            Serial.println();
+            Serial.println();
+            
+            
+            Serial.println(F("Message Content"));
+            Serial.print(F("    notesMessage["));
+            Serial.print(NOTES_MESSAGE_LEN);
+            Serial.print(F("]: "));
+            Serial.print(GetNotesMessage());
+            Serial.println();
+            
+            Serial.println(F("---------------------"));
+        }
+        
+    private:
+        char deviceId[DEVICE_ID_LEN + 1];
+        
+        char callsign[CALLSIGN_LEN + 1];
+        
+        uint32_t reportIntervalSecs = 60;
+        
+        uint8_t  transmitCount             = 2;
+        uint32_t delaySecsBetweenTransmits = 3;
+        
+        char notesMessage[NOTES_MESSAGE_LEN + 1];
+    };
+
+    
+public:
+
+    static uint8_t GetUserConfig(UserConfig &userConfig)
+    {
+        uint8_t retVal = 0;
+        
+        uint8_t configurationAvailableToValidate = 0;
+        
+        // Check serial in, if it's high, we assume a serial-speaker is on the
+        // other end.
+
+        // Was getting inconsistent readings here on startup.
+        // Wait briefly for pin state to settle.
+        PAL.Delay(20);
+        
+        if (PAL.DigitalRead(2))
+        {
+            Serial.println(F("Interactive configuration mode enabled."));
+            
+            configurationAvailableToValidate = GetUserConfigBySerial(userConfig);
+        }
+        else
+        {
+            Serial.println(F("No Serial-In detected, attempting to use previously-stored configuration."));
+            
+            EepromAccessor<UserConfig> ea;
+            if (ea.Read(userConfig))
+            {
+                Serial.println(F("OK: Previously-stored configuration found and loaded."));
+
+                configurationAvailableToValidate = 1;
+            }
+            else
+            {
+                Serial.println(F("ERR: No stored configuration found."));
+            }
+        }
+        
+        if (configurationAvailableToValidate)
+        {
+            // Check configuration
+            uint8_t errCount = 0;
+            
+            Serial.println(F("Validating configuration."));
+            
+            if (!strlen(userConfig.GetDeviceId()))
+            {
+                Serial.println(F("ERR: deviceId blank."));
+                
+                ++errCount;
+            }
+            
+            if (!strlen(userConfig.GetCallsign()))
+            {
+                Serial.println(F("ERR: callsign blank."));
+                
+                ++errCount;
+            }
+            
+            if (userConfig.GetReportIntervalSecs() == 0)
+            {
+                Serial.println(F("ERR: reportIntervalSecs can't be zero."));
+                
+                ++errCount;
+            }
+            
+            if (userConfig.GetTransmitCount() == 0)
+            {
+                Serial.println(F("ERR: transmitCount can't be zero."));
+                
+                ++errCount;
+            }
+            
+            if (!errCount)
+            {
+                retVal = 1;
+                
+                Serial.println(F("OK: Configuration valid."));
+            }
+            else
+            {
+                Serial.print(F("ERR: "));
+                Serial.print(errCount);
+                Serial.println(F(" configuration errors detected."));
+            }
+        }
+        else
+        {
+            Serial.println(F("ERR: No stored configuration found, no new configuration set."));
+        }
+        
+        return retVal;
+    }
+    
+private:
+    static uint8_t GetUserConfigBySerial(UserConfig &userConfig)
+    {
+        uint8_t userConfigAcquired = 0;
+        
+        EepromAccessor<UserConfig> ea;
+        userConfigAcquired = ea.Read(userConfig);
+        
+        if (userConfigAcquired)
+        {
+            Serial.println(F("Previously-stored configuration found and loaded."));
+        }
+        else
+        {
+            Serial.println(F("No Previously-stored configuration found, loading default values."));
+        }
+        
+        PrintHelp(userConfig);
+        
+        uint8_t cont = 1;
+        while (cont)
+        {
+            const uint8_t BUF_SIZE = 100;
+            char buf[BUF_SIZE];
+        
             uint8_t strLen = SerialReadLine(buf, BUF_SIZE);
             
             if (strLen)
@@ -32,15 +239,15 @@ public:
                 
                 uint8_t tokenCount = str.TokenCount(' ');
                 
-                uint8_t understood  = 1;
-                uint8_t updated     = 1;
+                uint8_t understood  = 0;
+                uint8_t updated     = 0;
                 
                 if (!strcmp_P(str.UnsafePtr(), PSTR("help")) ||
                     !strcmp_P(str.UnsafePtr(), PSTR("?")))
                 {
-                    PrintHelp();
+                    PrintHelp(userConfig);
                     
-                    updated = 0;
+                    understood = 1;
                 }
                 else if (tokenCount == 1)
                 {
@@ -51,12 +258,27 @@ public:
                         UserConfig userConfigTmp;
                         
                         ea.Write(userConfigTmp);
-                        ea.Read(userConfig_);
+                        ea.Read(userConfig);
+                        
+                        understood = 1;
+                        updated    = 1;
                     }
-                    else
+                    if (!strcmp_P(name, PSTR("delete")))
                     {
-                        understood  = 0;
-                        updated     = 0;
+                        // Debug only
+                        ea.Delete();
+                        
+                        Serial.println(F("Deleted EEPROM, restarting..."));
+                        Serial.println();
+                        PAL.Delay(100);
+                        
+                        PAL.SoftReset();
+                    }
+                    if (!strcmp_P(name, PSTR("run")))
+                    {
+                        cont = 0;
+                        
+                        understood = 1;
                     }
                 }
                 else if (tokenCount >= 2)
@@ -65,41 +287,46 @@ public:
                     
                     if (!strcmp_P(name, PSTR("deviceId")))
                     {
-                        userConfig_.SetDeviceId(str.UnsafePtrAtTokenAtIdx(1, ' '));
+                        userConfig.SetDeviceId(str.UnsafePtrAtTokenAtIdx(1, ' '));
+                        
+                        understood = 1;
+                        updated    = 1;
                     }
                     else if (!strcmp_P(name, PSTR("callsign")))
                     {
-                        userConfig_.SetCallsign(str.TokenAtIdx(1, ' '));
-                    }
-                    else if (!strcmp_P(name, PSTR("ssid")))
-                    {
-                        userConfig_.SetSsid(atoi(str.TokenAtIdx(1, ' ')));
+                        userConfig.SetCallsign(str.TokenAtIdx(1, ' '));
+                        
+                        understood = 1;
+                        updated    = 1;
                     }
                     else if (!strcmp_P(name, PSTR("reportIntervalSecs")))
                     {
-                        userConfig_.SetReportIntervalSecs(atol(str.TokenAtIdx(1, ' ')));
+                        userConfig.SetReportIntervalSecs(atol(str.TokenAtIdx(1, ' ')));
+                        
+                        understood = 1;
+                        updated    = 1;
                     }
                     else if (!strcmp_P(name, PSTR("transmitCount")))
                     {
-                        userConfig_.SetTransmitCount(atoi(str.TokenAtIdx(1, ' ')));
+                        userConfig.SetTransmitCount(atoi(str.TokenAtIdx(1, ' ')));
+                        
+                        understood = 1;
+                        updated    = 1;
                     }
                     else if (!strcmp_P(name, PSTR("delaySecsBetweenTransmits")))
                     {
-                        userConfig_.SetDelaySecsBetweenTransmits(atol(str.TokenAtIdx(1, ' ')));
+                        userConfig.SetDelaySecsBetweenTransmits(atol(str.TokenAtIdx(1, ' ')));
+                        
+                        understood = 1;
+                        updated    = 1;
                     }
                     else if (!strcmp_P(name, PSTR("notesMessage")))
                     {
-                        userConfig_.SetNotesMessage(str.UnsafePtrAtTokenAtIdx(1, ' '));
+                        userConfig.SetNotesMessage(str.UnsafePtrAtTokenAtIdx(1, ' '));
+                        
+                        understood = 1;
+                        updated    = 1;
                     }
-                    else
-                    {
-                        understood  = 0;
-                        updated     = 0;
-                    }
-                }
-                else
-                {
-                    understood = 0;
                 }
                 
                 if (!understood)
@@ -113,42 +340,39 @@ public:
                 
                 if (updated)
                 {
-                    ea.Write(userConfig_);
+                    Serial.println(F("Auto-saving changes."));
+                    ea.Write(userConfig);
+                    
+                    userConfigAcquired = 1;
                     
                     Serial.println();
-                    PrintCurrentState();
+                    userConfig.Print();
                     Serial.println();
                 }
             }
         }
+        
+        return userConfigAcquired;
     }
-
     
-    
-    
-    
-private:
-    
-    void PrintHelp()
+    static void PrintHelp(UserConfig &userConfig)
     {
         Serial.println();
         
-        PrintCurrentState();
+        userConfig.Print();
         
         Serial.println();
-        Serial.print(F("To override any parameter, type its name, space, and "));
-        Serial.print(F("a new value."));
-        Serial.println();
+        Serial.println(F("To set any parameter, type its name, space, and a new value"));
         Serial.println(F("Then hit enter."));
-        
         Serial.println(F("Any changes are saved automatically."));
         Serial.println();
+        
         Serial.println(F("To start over, type reset and hit enter."));
         Serial.println();
         
-        Serial.print(F("When finished, disconnect the Serial-In line "));
-        Serial.print(F("and then restart the device."));
-        Serial.println();
+        Serial.println(F("When finished, you can either:"));
+        Serial.println(F("- disconnect the Serial-In line and restart the device -or-"));
+        Serial.println(F("- type run and hit enter"));
         Serial.println();
         
         Serial.print(F("Type help or ? any time."));
@@ -156,73 +380,10 @@ private:
         
         Serial.println();
     }
-    
-
-
-    void PrintCurrentState()
-    {
-        Serial.println(F("Current Configuration"));
-        Serial.println(F("---------------------"));
-        
-        Serial.println(F("Device Unique Identification"));
-        
-        Serial.print(F("    deviceId["));
-        Serial.print(UserConfig::DEVICE_ID_LEN);
-        Serial.print(F("]: "));
-        Serial.print(userConfig_.GetDeviceId());
-        Serial.println();
-        Serial.println();
-        
-        
-        Serial.println(F("Your Identity"));
-        
-        Serial.print(F("    callsign["));
-        Serial.print(UserConfig::CALLSIGN_LEN);
-        Serial.print(F("]: "));
-        Serial.print(userConfig_.GetCallsign());
-        Serial.println();
-        
-        Serial.print(F("    ssid: "));
-        Serial.print(userConfig_.GetSsid());
-        Serial.println();
-        Serial.println();
-        
-        
-        Serial.println(F("Reporting Interval"));
-        
-        Serial.print(F("    reportIntervalSecs: "));
-        Serial.print(userConfig_.GetReportIntervalSecs());
-        Serial.println();
-        Serial.println();
-        
-        
-        Serial.println(F("Reliability of Transmission"));
-        
-        Serial.print(F("    transmitCount: "));
-        Serial.print(userConfig_.GetTransmitCount());
-        Serial.println();
-        
-        Serial.print(F("    delaySecsBetweenTransmits: "));
-        Serial.print(userConfig_.GetDelaySecsBetweenTransmits());
-        Serial.println();
-        Serial.println();
-        
-        
-        Serial.println(F("Message Content"));
-        Serial.print(F("    notesMessage["));
-        Serial.print(UserConfig::NOTES_MESSAGE_LEN);
-        Serial.print(F("]: "));
-        Serial.print(userConfig_.GetNotesMessage());
-        Serial.println();
-        
-        Serial.println(F("---------------------"));
-    }
-
-    UserConfig userConfig_;
 };
 
 
-#endif  // __APP_PICO_TRACKER_1_USER_CONFIG_SERIAL_INTERFACE_H__
+#endif  // __APP_PICO_TRACKER_1_USER_CONFIG_MANAGER_H__
 
 
 
