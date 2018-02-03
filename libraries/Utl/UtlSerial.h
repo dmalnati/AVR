55502424bfc9ab88d2b92ee483cef10d7784fa74
dmalnati <douglas.malnati@gmail.com>
2018-02-02 22:27:00 -0500

    AVR UtlSerial buffer overflow fix

--- libraries/Utl/UtlSerial.h
+++ libraries/Utl/UtlSerial.h
@@ -10,9 +10,28 @@ uint8_t SerialReadLine(char *buf, uint8_t bufSize)
     {
         retVal = Serial.readBytesUntil('\n', buf, bufSize);
 
-        if (retVal)
+        if (retVal < bufSize)
         {
             buf[retVal] = '\0';
+        }
+        else
+        {
+            buf[bufSize - 1] = '\0';
+            
+            // newline wasn't hit, and bytes weren't discarded, so consume them
+            uint8_t cont = 1;
+            while (cont)
+            {
+                if (Serial.available())
+                {
+                    char c = Serial.read();
+                    
+                    if (c == '\n')
+                    {
+                        cont = 0;
+                    }
+                }
+            }
         }
     }
     
