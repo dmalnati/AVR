b7b345b27c9a60b15c2b9cdcaa62aadd1707f70f
dmalnati <douglas.malnati@gmail.com>
2018-02-06 00:37:07 -0500

    AVR SensorGPSUblox now requires more direct control, less defaulted

--- libraries/Peripheral/SensorGPSUblox.h
+++ libraries/Peripheral/SensorGPSUblox.h
@@ -24,7 +24,7 @@ public:
 private:
     static const uint32_t BAUD                  = 9600;
     static const uint32_t POLL_PERIOD_MS        = 25;
-    static const uint16_t GPS_WARMUP_TIME_MS    = 2000;
+    static const uint16_t GPS_WARMUP_TIME_MS    = 100;
     static const uint8_t  MAX_UBX_MESSAGE_SIZE  = 44;
     
 public:
@@ -35,27 +35,21 @@ public:
         // nothing to do
     }
     
-    void Init(uint16_t messageIntervalMs = 0)
+    // Debug
+    SoftwareSerial &DebugGetSS()
+    {
+        return ss_;
+    }
+    
+    // All configuration has to take place post-Init, as messages are relayed
+    // to the GPS, and that isn't possible until after Init.
+    void Init()
     {
         // Give the GPS a moment to warm up and do stuff
         PAL.Delay(GPS_WARMUP_TIME_MS);
-        
-        // Start serial comms
-        ss_.begin(BAUD);
-        ss_.listen();
-        
-        // Configure GPS
-        SetHighAltitudeMode();
-        EnableOnlyGGAAndRMC();
-        
-        if (messageIntervalMs)
-        {
-            SetMessageInterval(messageIntervalMs);
-        }
-        
-        // Set timers to come read serial data periodically
-        timer_.SetCallback([this]() { this->OnTimeout(); });
-        timer_.RegisterForTimedEventInterval(POLL_PERIOD_MS);
+
+        // Interface with serial
+        EnableSerialInput();
     }
     
     void DisableSerialInput()
@@ -67,6 +61,88 @@ public:
     {
         ss_.begin(BAUD);
         ss_.listen();
+        
+        // Set timers to come read serial data periodically
+        timer_.SetCallback([this]() { this->OnTimeout(); });
+        timer_.RegisterForTimedEventInterval(POLL_PERIOD_MS);
+    }
+    
+    void SetHighAltitudeMode()
+    {
+        ubxMessage_.Reset();
+        
+        // Create CFG-NAV5 msg
+        // Set altitude mode to Airborne < 1g
+        // Indicate this is the only parameter changing via bitmap
+        ubxMessage_.SetClass(0x06);
+        ubxMessage_.SetId(0x24);
+        
+        ubxMessage_.AddFieldX2(0x0001); // mask     = dynModel change only
+        ubxMessage_.AddFieldU1(6);      // dynModel = airborne < 1g
+        ubxMessage_.AddFieldU1(0);      // fixMode
+        ubxMessage_.AddFieldI4(0);      // fixedAlt
+        ubxMessage_.AddFieldU4(0);      // fixedAltVar
+        ubxMessage_.AddFieldI1(0);      // minElev
+        ubxMessage_.AddFieldU1(0);      // drLimit
+        ubxMessage_.AddFieldU2(0);      // pDop
+        ubxMessage_.AddFieldU2(0);      // tDop
+        ubxMessage_.AddFieldU2(0);      // pAcc
+        ubxMessage_.AddFieldU2(0);      // tAcc
+        ubxMessage_.AddFieldU1(0);      // staticHoldThresh
+        ubxMessage_.AddFieldU1(0);      // dgpsTimeOut
+        ubxMessage_.AddFieldU4(0);      // reserved2
+        ubxMessage_.AddFieldU4(0);      // reserved3
+        ubxMessage_.AddFieldU4(0);      // reserved4
+        
+        uint8_t *buf;
+        uint8_t  bufLen;
+        ubxMessage_.GetBuf(&buf, &bufLen);
+        
+        ss_.write(buf, bufLen);
+    }
+
+    void SetMessageInterval(uint16_t intervalMs)
+    {
+        ubxMessage_.Reset();
+        
+        // Create CFG-RATE msg
+        ubxMessage_.SetClass(0x06);
+        ubxMessage_.SetId(0x08);
+        
+        ubxMessage_.AddFieldU2(intervalMs); // measRate
+        ubxMessage_.AddFieldU2(1);          // navRate
+        ubxMessage_.AddFieldU2(1);          // timeRef
+        
+        uint8_t *buf;
+        uint8_t  bufLen;
+        ubxMessage_.GetBuf(&buf, &bufLen);
+        
+        ss_.write(buf, bufLen);
+    }
+    
+    void EnableOnlyGGAAndRMC()
+    {
+        // These are the messages broadcast by default.
+        // Specifically enable and disable to be certain.
+        uint8_t ubxClassUbxIdRateList[] = {
+            0xF0, 0x00, 1,  // GGA
+            0xF0, 0x01, 0,  // GLL
+            0xF0, 0x02, 0,  // GSA
+            0xF0, 0x03, 0,  // GSV
+            0xF0, 0x04, 1,  // RMC
+            0xF0, 0x05, 0   // VTG
+        };
+        
+        uint8_t ubxClassUbxIdRateListLen = sizeof(ubxClassUbxIdRateList);
+        
+        for (uint8_t i = 0; i < ubxClassUbxIdRateListLen; i += 3)
+        {
+            uint8_t ubxClass = ubxClassUbxIdRateList[i + 0];
+            uint8_t ubxId    = ubxClassUbxIdRateList[i + 1];
+            uint8_t rate     = ubxClassUbxIdRateList[i + 2];
+            
+            SetMessageRate(ubxClass, ubxId, rate);
+        }
     }
     
     struct Measurement
@@ -155,83 +231,6 @@ private:
         }
     }
     
-    void SetHighAltitudeMode()
-    {
-        ubxMessage_.Reset();
-        
-        // Create CFG-NAV5 msg
-        // Set altitude mode to Airborne < 1g
-        // Indicate this is the only parameter changing via bitmap
-        ubxMessage_.SetClass(0x06);
-        ubxMessage_.SetId(0x24);
-        
-        ubxMessage_.AddFieldX2(0x0001); // mask     = dynModel change only
-        ubxMessage_.AddFieldU1(6);      // dynModel = airborne < 1g
-        ubxMessage_.AddFieldU1(0);      // fixMode
-        ubxMessage_.AddFieldI4(0);      // fixedAlt
-        ubxMessage_.AddFieldU4(0);      // fixedAltVar
-        ubxMessage_.AddFieldI1(0);      // minElev
-        ubxMessage_.AddFieldU1(0);      // drLimit
-        ubxMessage_.AddFieldU2(0);      // pDop
-        ubxMessage_.AddFieldU2(0);      // tDop
-        ubxMessage_.AddFieldU2(0);      // pAcc
-        ubxMessage_.AddFieldU2(0);      // tAcc
-        ubxMessage_.AddFieldU1(0);      // staticHoldThresh
-        ubxMessage_.AddFieldU1(0);      // dgpsTimeOut
-        ubxMessage_.AddFieldU4(0);      // reserved2
-        ubxMessage_.AddFieldU4(0);      // reserved3
-        ubxMessage_.AddFieldU4(0);      // reserved4
-        
-        uint8_t *buf;
-        uint8_t  bufLen;
-        ubxMessage_.GetBuf(&buf, &bufLen);
-        
-        ss_.write(buf, bufLen);
-    }
-
-    void SetMessageInterval(uint16_t intervalMs)
-    {
-        ubxMessage_.Reset();
-        
-        // Create CFG-RATE msg
-        ubxMessage_.SetClass(0x06);
-        ubxMessage_.SetId(0x08);
-        
-        ubxMessage_.AddFieldU2(intervalMs); // measRate
-        ubxMessage_.AddFieldU2(1);          // navRate
-        ubxMessage_.AddFieldU2(1);          // timeRef
-        
-        uint8_t *buf;
-        uint8_t  bufLen;
-        ubxMessage_.GetBuf(&buf, &bufLen);
-        
-        ss_.write(buf, bufLen);
-    }
-    
-    void EnableOnlyGGAAndRMC()
-    {
-        // These are the messages broadcast by default.
-        // Specifically enable and disable to be certain.
-        uint8_t ubxClassUbxIdRateList[] = {
-            0xF0, 0x00, 1,  // GGA
-            0xF0, 0x01, 0,  // GLL
-            0xF0, 0x02, 0,  // GSA
-            0xF0, 0x03, 0,  // GSV
-            0xF0, 0x04, 1,  // RMC
-            0xF0, 0x05, 0   // VTG
-        };
-        
-        uint8_t ubxClassUbxIdRateListLen = sizeof(ubxClassUbxIdRateList);
-        
-        for (uint8_t i = 0; i < ubxClassUbxIdRateListLen; i += 3)
-        {
-            uint8_t ubxClass = ubxClassUbxIdRateList[i + 0];
-            uint8_t ubxId    = ubxClassUbxIdRateList[i + 1];
-            uint8_t rate     = ubxClassUbxIdRateList[i + 2];
-            
-            SetMessageRate(ubxClass, ubxId, rate);
-        }
-    }
     
     void SetMessageRate(uint8_t ubxClass, uint8_t ubxId, uint8_t rate)
     {
