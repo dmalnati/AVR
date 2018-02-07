573d27c48035acff3222d92665418ea22f4bc4ae
dmalnati <douglas.malnati@gmail.com>
2018-02-07 01:13:09 -0500

    AVR TestSensorGPSUbloxPowerSave can parse response messages.  Tested almanac and ephemeris requests

--- sketches/TestSensorGPSUbloxPowerSave/TestSensorGPSUbloxPowerSave.ino
+++ sketches/TestSensorGPSUbloxPowerSave/TestSensorGPSUbloxPowerSave.ino
@@ -2,6 +2,7 @@
 #include "SoftwareSerial.h"
 #include "SensorGPSUblox.h"
 #include "UtlSerial.h"
+#include "UtlStreamBlob.h"
 
 
 // Not actually used, just needs a concrete instance
@@ -20,6 +21,288 @@ static SerialShell<NUM_COMMANDS> shell;
 
 static UbxMessage<100> ubxMsg;
 
+static const uint8_t UBX_IN_BUF_SIZE = 150;
+static uint8_t ubxInBuf[UBX_IN_BUF_SIZE] = { 0 };
+
+
+struct ParsedMessage
+{
+    uint8_t  msgClass;
+    uint8_t  msgId;
+    uint8_t *buf;
+    uint8_t  len;
+    uint8_t  bufLen;
+    
+    const char *failReason;
+};
+
+
+void PrintMessage(ParsedMessage msg)
+{
+    Serial.print(F("msgClass: 0x")); Serial.println(msg.msgClass, HEX);
+    Serial.print(F("msgId   : 0x")); Serial.println(msg.msgId, HEX);
+    Serial.print(F("msglen  : ")); Serial.println(msg.len);
+    Serial.print(F("bufLen  : ")); Serial.println(msg.bufLen);
+    StreamBlob(Serial, msg.buf, msg.bufLen, 1, 1);
+}
+
+uint8_t GetMessage(ParsedMessage &msg)
+{
+    enum class State : uint8_t
+    {
+        LOOKING_FOR_HEADER = 0,
+        LOOKING_FOR_CLASS,
+        LOOKING_FOR_ID,
+        LOOKING_FOR_LEN,
+        LOOKING_FOR_CHECKSUM,
+    };
+
+    State state = State::LOOKING_FOR_HEADER;
+
+    uint8_t retVal = 0;
+
+    static const uint16_t GIVE_UP_AFTER = 500;
+    uint16_t tryCount = 0;
+
+    uint8_t idx = 0;
+
+    uint8_t msgClass = 0;
+    uint8_t msgId    = 0;
+    uint8_t bufLen   = 0;
+
+    uint16_t len = 0;
+
+    uint8_t idxStopAt = 0;
+
+    uint8_t found = 0;
+
+    uint8_t cont = 1;
+    while (cont)
+    {
+        if (ss.available())
+        {
+            //Serial.println(F("Available"));
+            
+            uint8_t b = (uint8_t)ss.read();
+
+            if (state == State::LOOKING_FOR_HEADER)
+            {
+                //Serial.println(F("LOOKING_FOR_HEADER"));
+                
+                // store this byte
+                ubxInBuf[idx] = b;
+                ++idx;
+
+                // check if we received a byte previously
+                // need 2 to match header
+                if (idx == 1)
+                {
+                    // nope, leave that byte stored and carry on
+                }
+                else
+                {
+                    // yup, check if this is a valid header now
+                    if (ubxInBuf[0] == 0xB5 && ubxInBuf[1] == 0x62)
+                    {
+                        // yup, move to next state
+                        state = State::LOOKING_FOR_CLASS;
+                    }
+                    else
+                    {
+                        //Serial.print(F("    discarding ")); Serial.println(ubxInBuf[0], HEX);
+                        
+                        // nope, maybe the last byte that came in is the start,
+                        // shift it to the start and carry on
+                        ubxInBuf[0] = ubxInBuf[1];
+                        --idx;
+                    }
+                }
+            }
+            else if (state == State::LOOKING_FOR_CLASS)
+            {
+                //Serial.println(F("LOOKING_FOR_CLASS"));
+
+                ubxInBuf[idx] = b;
+                ++idx;
+                
+                msgClass = b;
+
+                //Serial.print(F("    class: "));  Serial.println(b, HEX);
+
+                state = State::LOOKING_FOR_ID;
+            }
+            else if (state == State::LOOKING_FOR_ID)
+            {
+                //Serial.println("LOOKING_FOR_ID");
+
+                ubxInBuf[idx] = b;
+                ++idx;
+                
+                msgId = b;
+
+                //Serial.print(F("    id: "));  Serial.println(b, HEX);
+
+                state = State::LOOKING_FOR_LEN;
+            }
+            else if (state == State::LOOKING_FOR_LEN)
+            {
+                //Serial.println(F("LOOKING_FOR_LEN"));
+                
+                ubxInBuf[idx] = b;
+                ++idx;
+
+                if (idx == 6)
+                {
+                    // we have the full size
+                    // we want to go from little endian wire format to host endian
+                    // start by making a network-byte-order 16-bit int, aka big endian
+                    uint16_t lenBigEndian;
+                    char *p = (char *)&lenBigEndian;
+                    p[0] = ubxInBuf[5];
+                    p[1] = ubxInBuf[4];
+
+                    len = PAL.ntohs(lenBigEndian);
+
+                    //Serial.print(F("    lenBigEndian: "));  Serial.println(lenBigEndian);
+                    //Serial.print(F("    len         : "));  Serial.println(len);
+
+                    // length does not include the header, class, id, length, or checksum fields.
+                    if (idx + len + 2 <= UBX_IN_BUF_SIZE)
+                    {
+                        idxStopAt = idx + len + 2;
+
+                        state = State::LOOKING_FOR_CHECKSUM;
+                    }
+                    else
+                    {
+                        // Can't fit
+                        cont = 0;
+
+                        //Serial.println(F("Message too large"));
+                    }
+                }
+                else
+                {
+                    // Nothing to do, keep collecting
+                }
+            }
+            else if (state == State::LOOKING_FOR_CHECKSUM)
+            {
+                //Serial.println(F("LOOKING_FOR_CHECKSUM"));
+                
+                ubxInBuf[idx] = b;
+                ++idx;
+
+                if (idx == idxStopAt)
+                {
+                    // Time to calculate and compare the checksum
+
+                    uint8_t idxChecksumStart = idxStopAt - 2;
+
+                    uint8_t ckA = 0;
+                    uint8_t ckB = 0;
+
+                    // did store the header (2 bytes total)
+                    // did store the class and id (2 bytes total)
+                    // did store the size (2 bytes total)
+                    // supposed to checksum from class and id forward, 
+                    
+                    for (uint8_t i = 2; i < idxChecksumStart; ++i)
+                    {
+                        uint8_t b = ubxInBuf[i];
+                        
+                        ckA += b;
+                        ckB += ckA;
+                    }
+
+                    // extract the message checksum
+                    uint8_t msgCkA = ubxInBuf[idxChecksumStart + 0];
+                    uint8_t msgCkB = ubxInBuf[idxChecksumStart + 1];
+
+                    //Serial.print("ckA, ckB: ");       Serial.print(ckA);    Serial.print(" "); Serial.print(ckB);    Serial.println();
+                    //Serial.print("msgCkA, msgCkB: "); Serial.print(msgCkA); Serial.print(" "); Serial.print(msgCkB); Serial.println();
+
+                    //StreamBlob(Serial, ubxInBuf, UBX_IN_BUF_SIZE, 1, 1);
+                    bufLen = idxStopAt;
+
+                    if (ckA == msgCkA && ckB == msgCkB)
+                    {
+                        // success
+                        found = 1;
+                    }
+                    else
+                    {
+                        //Serial.println(F("Checksum failed"));
+                        msg.failReason = "Checksum failed";
+                    }
+
+                    cont = 0;
+                }
+                else
+                {
+                    // Nothing to do, just pile on bytes until reaching the checksum
+                }
+            }
+        }
+        else
+        {
+            PAL.Delay(1);
+        }
+
+        ++tryCount;
+        if (tryCount == GIVE_UP_AFTER)
+        {
+            cont = 0;
+
+            //Serial.println(F("Too many attempts, giving up"));
+            msg.failReason = "Too many attempts";
+        }
+    }
+
+    if (found)
+    {
+        retVal = 1;
+
+        msg.msgClass = msgClass;
+        msg.msgId    = msgId;
+        msg.buf      = ubxInBuf;
+        msg.len      = len;
+        msg.bufLen   = bufLen;
+        
+        msg.failReason = "";
+    }
+
+    return retVal;
+}
+
+uint8_t GetMessageOrErr(uint8_t printMessage = 1)
+{
+    uint8_t retVal = 0;
+    
+    ParsedMessage msg;
+
+    Serial.print("Waiting for msg... ");
+    if (GetMessage(msg))
+    {
+        retVal = 1;
+        
+        Serial.println("YES");
+
+        if (printMessage)
+        {
+            PrintMessage(msg);
+        }
+    }
+    else
+    {
+        Serial.print("NO - ");
+        Serial.print(msg.failReason);
+        Serial.println();
+    }
+    Serial.println();
+
+    return retVal;
+}
 
 void setup()
 {
@@ -28,21 +311,27 @@ void setup()
 
     gps.Init();
     gps.SetHighAltitudeMode();
+    GetMessageOrErr();
 
-    shell.RegisterCommand("lowpower", [](char *) {
-        Serial.println("Enabling low power mode");
-        
+    // turns out this doesn't work on my model
+    shell.RegisterCommand("noglonass", [](char *) {
+        Serial.println("Disabling GLONASS");
         // Disable GLONASS mode
         static uint8_t disable_glonass[20] = {0xB5, 0x62, 0x06, 0x3E, 0x0C, 0x00, 0x00, 0x00, 0x20, 0x01, 0x06, 0x08, 0x0E, 0x00, 0x00, 0x00, 0x01, 0x01, 0x8F, 0xB2};
     
-        //gps_sendubx(disable_glonass, 20);
         ss.write(disable_glonass, 20);
-    
+
+        GetMessageOrErr();
+    });
+
+    shell.RegisterCommand("lowpower", [](char *) {
+        Serial.println("Enabling power save");
         // Enable power saving
         uint8_t enable_powersave[10] = {0xB5, 0x62, 0x06, 0x11, 0x02, 0x00, 0x08, 0x01, 0x22, 0x92};
         
-        //gps_sendubx(enable_powersave, 10);
         ss.write(enable_powersave, 10);
+
+        GetMessageOrErr();
     });
 
     shell.RegisterCommand("interval", [](char *cmdStr) {
@@ -55,6 +344,8 @@ void setup()
             Serial.print("Setting interval to ");  Serial.println(intervalMs);
             
             gps.SetMessageInterval(intervalMs);
+            
+            GetMessageOrErr();
         }
     });
 
@@ -93,6 +384,7 @@ void setup()
         // Fill out UBX Message
         ubxMsg.Reset();
 
+        // CFG-RST (0x06 0x04)
         ubxMsg.SetClass(0x06);
         ubxMsg.SetId(0x04);
 
@@ -105,9 +397,63 @@ void setup()
         ubxMsg.GetBuf(&buf, &bufLen);
 
         ss.write(buf, bufLen);
+
+        GetMessageOrErr();  // Didn't see this work ever
+    });
+
+    shell.RegisterCommand("alm", [](char *) {
+        ubxMsg.Reset();
+
+        // AID-ALM (0x0B 0x30)
+        ubxMsg.SetClass(0x0B);
+        ubxMsg.SetId(0x30);
+
+        uint8_t *buf;
+        uint8_t  bufLen;
+        ubxMsg.GetBuf(&buf, &bufLen);
+
+        ss.write(buf, bufLen);
+
+        // Get all messages
+        uint8_t count = 0;
+        while (GetMessageOrErr(0))
+        {
+            ++count;
+        }
+        Serial.print(F("Got ")); Serial.print(count); Serial.println(F(" AID-ALM responses"));
+    });
+
+    shell.RegisterCommand("eph", [](char *) {
+        ubxMsg.Reset();
+
+        // AID-EPH (0x0B 0x31)
+        ubxMsg.SetClass(0x0B);
+        ubxMsg.SetId(0x31);
+
+        uint8_t *buf;
+        uint8_t  bufLen;
+        ubxMsg.GetBuf(&buf, &bufLen);
+
+        ss.write(buf, bufLen);
+
+        // Get all messages
+        uint8_t count = 0;
+        while (GetMessageOrErr(0))
+        {
+            ++count;
+        }
+        Serial.print(F("Got ")); Serial.print(count); Serial.println(F(" AID-EPH responses"));
     });
 
     
+
+
+    shell.RegisterErrorHandler([](char *cmdStr) {
+        Serial.print("ERR: Unrecognized \"");
+        Serial.print(cmdStr);
+        Serial.print("\"");
+        Serial.println();
+    });
     
     while (1)
     {
