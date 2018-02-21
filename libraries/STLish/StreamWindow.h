09d5199b192bd4dc7e1dd181608c87d1a4e4a0ea
dmalnati <douglas.malnati@gmail.com>
2018-02-20 22:18:37 -0500

    AVR StreamWindow working

--- libraries/STLish/StreamWindow.h
+++ libraries/STLish/StreamWindow.h
@@ -2,29 +2,171 @@
 #define __STREAM_WINDOW_H__
 
 
-
 /*
-
-Design objectives
-- constantly add characters
-- old ones get pushed out as buffer fills
-- users can keep/get pointer to start and use as string
-  - meaning null terminator
-  - meaning inefficient constantly shifting left, but who cares
-- uses its own buffer or someone else's?
-  - could do both, but which first?
-- ultimately to be used by async serial input for getline
-
-
+ * Design objectives
+ * - constantly add elements
+ * - old ones get pushed out as buffer fills
+ * - uses someone else's buffer
+ * 
  */
 
+template <typename T>
 class StreamWindow
 {
+public:
+    StreamWindow()
+    {
+        Attach(NULL, 0);
+    }
     
+    StreamWindow(T *arr, uint8_t elementCount, T filler = T{})
+    {
+        Attach(arr, elementCount, filler);
+    }
+    
+    void Attach(T *arr, uint8_t elementCount, T filler = T{})
+    {
+        arr_          = NULL;
+        arrSize_      = 0;
+        elementCount_ = 0;
+        filler        = T{};
+        
+        if (arr && elementCount)
+        {
+            arr_          = arr;
+            arrSize_      = 0;
+            elementCount_ = elementCount;
+            filler_       = filler;
+            
+            Reset();
+        }
+    }
+    
+    void Reset()
+    {
+        if (arr_)
+        {
+            for (uint8_t i = 0; i < elementCount_; ++i)
+            {
+                arr_[i] = filler_;
+            }
+            
+            arrSize_ = 0;
+        }
+    }
+    
+    uint8_t Append(T &val)
+    {
+        uint8_t retVal = 0;
+        
+        if (arr_)
+        {
+            retVal = MakeRoomForOneMore();
+            
+            if (retVal)
+            {
+                arr_[arrSize_] = val;
+                ++arrSize_;
+            }
+        }
+        
+        return retVal;
+    }
+    
+    uint8_t Size()
+    {
+        uint8_t retVal = 0;
+        
+        if (arr_)
+        {
+            retVal = arrSize_;
+        }
+        
+        return retVal;
+    }
+    
+    uint8_t Capacity()
+    {
+        uint8_t retVal = 0;
+        
+        if (arr_)
+        {
+            retVal = elementCount_;
+        }
+    
+        return retVal;
+    }
+    
+
+private:
+    
+    uint8_t MakeRoomForOneMore()
+    {
+        uint8_t retVal = 0;
+        
+        if (arr_)
+        {
+            retVal = 1;
+            
+            if (arrSize_ == elementCount_)
+            {
+                // out of space, shift old out
+                for (uint8_t i = 0; i < elementCount_; ++i)
+                {
+                    arr_[i] = arr_[i + 1];
+                }
+                
+                // update buffer size to be one less for shifted-out element
+                --arrSize_;
+                
+                // fill in hole
+                arr_[arrSize_] = filler_;
+            }
+        }
+        
+        return retVal;
+    }
+
+
+    T       *arr_;
+    uint8_t  arrSize_;
+    uint8_t  elementCount_;
+    T        filler_;
 };
 
 
 #endif  // __STREAM_WINDOW_H__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ sketches/TestStreamWindow/TestStreamWindow.ino
@@ -0,0 +1,82 @@
+#include "StreamWindow.h"
+#include "UtlSerial.h"
+
+
+void Print(StreamWindow<char> &sw)
+{
+    Serial.print("Cap : "); Serial.println(sw.Capacity());
+    Serial.print("Size: "); Serial.println(sw.Size());
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting");
+
+    //
+    // Here we're going to:
+    // - allocate a buffer
+    // - fill last element with a NULL
+    // - tell stream window:
+    //   - here is the buffer
+    //   - it is one element smaller than we know it is
+    //   - make sure to fill in any unused elements with NULL
+    //
+    // This gets us a c-string regardless of how full it gets
+    //
+    const uint8_t BUF_SIZE = 12;
+    char buf[BUF_SIZE];
+    buf[BUF_SIZE - 1] = '\0';
+
+    StreamWindow<char> sw(buf, BUF_SIZE - 1, '\0');
+
+    Serial.print("Buf size: "); Serial.println(BUF_SIZE);
+    Serial.print("  buf: \""); Serial.print(buf); Serial.println("\"");
+    Serial.println("Initial StreamWindow:");
+    Print(sw);
+    Serial.println();
+
+
+
+    SerialShell<1> shell;
+
+    // The "error handler" here is just to catch arbitrary input
+    shell.RegisterErrorHandler([&](char *cmdStr){
+        Serial.print("Got: \"");
+        Serial.print(cmdStr);
+        Serial.print("\", len: ");
+        Serial.print(strlen(cmdStr));
+        Serial.println();
+
+        for (uint8_t i = 0; i < strlen(cmdStr); ++i)
+        {
+            Serial.print("Adding: \""); Serial.print(cmdStr[i]); Serial.println("\"");
+            sw.Append(cmdStr[i]);
+            Print(sw);
+            Serial.print("  buf: \""); Serial.print(buf); Serial.println("\"");
+            Serial.println();
+        }
+
+        Serial.println("Consumed");
+    });
+
+    shell.RegisterCommand("reset", [&](char *){
+        Serial.println("Resetting");
+        sw.Reset();
+        Print(sw);
+        Serial.print("  buf: \""); Serial.print(buf); Serial.println("\"");
+    });
+
+    
+
+    shell.Run();
+}
+
+void loop() {}
+
+
+
+
+
+
+
