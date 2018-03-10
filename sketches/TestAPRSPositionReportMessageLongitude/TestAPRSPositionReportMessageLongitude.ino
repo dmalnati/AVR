ef90e52a7b73d84b4ca0a14c4af3a8170c5c4ab0
dmalnati <douglas.malnati@gmail.com>
2018-03-10 18:51:14 -0500

    AVR test code looking for longitude bug, turned out to be in analysis TCL script

---
+++ sketches/TestAPRSPositionReportMessageLongitude/TestAPRSPositionReportMessageLongitude.ino
@@ -0,0 +1,66 @@
+#include "PAL.h"
+#include "Evm.h"
+#include "UtlSerial.h"
+#include "APRSPositionReportMessage.h"
+
+
+static Evm::Instance<10,10,10>         evm;
+static SerialAsyncConsoleEnhanced<10>  console;
+
+
+void TestLongitude(int16_t degrees, uint8_t minutes, double seconds)
+{
+    const uint8_t BUF_SIZE = 100;
+    char buf[BUF_SIZE];
+
+    memset(buf, ' ', BUF_SIZE);
+
+    APRSPositionReportMessage aprm;
+    
+    aprm.SetTargetBuf((uint8_t *)buf, BUF_SIZE);
+
+    aprm.SetLongitude(degrees, minutes, seconds);
+
+    Serial.print("Buf: \"");
+    Serial.write(buf, BUF_SIZE);
+    Serial.print("\"");
+    Serial.println();
+    
+    Serial.print("Bytes: "); Serial.println(aprm.GetBytesUsed());
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting");
+
+    console.RegisterCommand("test", [](char *cmdStr){
+        Str str(cmdStr);
+        
+        if (str.TokenCount(' ') == 4)
+        {
+            int16_t degrees = atol(str.TokenAtIdx(1, ' '));
+            uint8_t minutes = atoi(str.TokenAtIdx(2, ' '));
+            double  seconds = atof(str.TokenAtIdx(3, ' '));
+
+            Serial.print(F("Testing: "));
+            Serial.print(degrees);
+            Serial.print(" ");
+            Serial.print(minutes);
+            Serial.print(" ");
+            Serial.print(seconds);
+            Serial.println();
+
+            TestLongitude(degrees, minutes, seconds);
+        }
+    });
+
+    console.Start();
+
+    evm.MainLoop();
+}
+
+void loop() {}
+
+
+
---
+++ sketches/TestStrFormat/TestStrFormat.ino
@@ -0,0 +1,44 @@
+#include "PAL.h"
+#include "Evm.h"
+#include "UtlSerial.h"
+#include "StrFormat.h"
+
+
+static Evm::Instance<10,10,10>         evm;
+static SerialAsyncConsoleEnhanced<10>  console;
+
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting");
+
+    console.RegisterCommand("test", [](char *cmdStr){
+        Str str(cmdStr);
+        
+        if (str.TokenCount(' ') == 2)
+        {
+            uint32_t val = atol(str.TokenAtIdx(1, ' '));
+
+            Serial.print(F("Testing: ")); Serial.println(val);
+
+            char buf[3];
+            buf[2] = '\0';
+            
+            U32ToStrPadLeft(buf, val, 2, '0');
+
+            Serial.print(F("Result: ")); Serial.println(buf);
+        }
+    });
+
+    console.Start();
+
+    // process events
+    evm.MainLoop();
+}
+
+void loop() {}
+
+
+
+
