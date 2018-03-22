6c4e1ede5bfbe29a2153ec41699e7e451319abe3
dmalnati <douglas.malnati@gmail.com>
2018-03-21 21:19:59 -0400

    AVR TestLowPowerModeSleepOnly

---
+++ sketches/TestLowPowerModeSleepOnly/TestLowPowerModeSleepOnly.ino
@@ -0,0 +1,26 @@
+#include "PAL.h"
+
+
+static const uint32_t SPIN_MS  = 5000;
+static const uint32_t SLEEP_MS = 30000;
+
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting");
+    
+    // Just burn regular CPU on startup so it's easy to spot
+    // the difference when going low power
+    PAL.Delay(SPIN_MS);
+
+    // Now go low power forever
+    while (1)
+    {
+        PAL.DelayLowPower(SLEEP_MS);
+    }
+}
+
+void loop() {}
+
+
