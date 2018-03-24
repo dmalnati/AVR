53caba983cd417eee1068c7473add90e2c69f3dc
dmalnati <douglas.malnati@gmail.com>
2018-03-24 12:48:40 -0400

    AVR AppMultimeter initial checkin, 16 analog chans working

---
+++ libraries/AppMultimeter/AppMultimeter.h
@@ -0,0 +1,203 @@
+#ifndef __APP_MULTIMETER_H__
+#define __APP_MULTIMETER_H__
+
+
+#include "Eeprom.h"
+#include "Evm.h"
+#include "MuxAnalogDigitalCD74HC4067.h"
+#include "UtlSerial.h"
+
+
+struct AppMultimeterConfig
+{
+    // Pins used for multiplexer to indicate channel
+    uint8_t pinMuxBit0;
+    uint8_t pinMuxBit1;
+    uint8_t pinMuxBit2;
+    uint8_t pinMuxBit3;
+    
+    // Pin doing actual ADC
+    uint8_t pinAdc;
+};
+
+class AppMultimeter
+{
+private:
+    static const uint32_t DEFAULT_SAMPLE_RATE_MS = 1000;
+    
+    struct PersistentConfiguration
+    {
+        uint32_t SAMPLE_RATE_MS;
+    };
+    
+private:
+    static const uint8_t C_IDLE  =  0;
+    static const uint8_t C_TIMED = 20;
+    static const uint8_t C_INTER =  0;
+    
+public:
+    AppMultimeter(AppMultimeterConfig &cfg)
+    : cfg_(cfg)
+    , adcMux_(cfg_.pinMuxBit0,
+              cfg_.pinMuxBit1,
+              cfg_.pinMuxBit2,
+              cfg_.pinMuxBit3,
+              cfg_.pinAdc)
+    {
+        // Nothing to do
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    // Entry point
+    //
+    ///////////////////////////////////////////////////////////////////////////
+
+    void Run()
+    {
+        // Init serial and announce startup
+        Serial.begin(9600);
+        Serial.println(F("Starting"));
+        
+        // Set up sample rate based either on default or prior stored value
+        LoadSampleRate();
+        Serial.print("Sample rate: "); Serial.println(pc_.SAMPLE_RATE_MS);
+        
+        // Set up user input handling
+        SetUpUserInputHandlers();
+        
+        // Set up reporting
+        tedSample_.SetCallback([this](){ OnSample(); });
+        tedSample_.RegisterForTimedEventInterval(GetSampleRate(), 0);
+        
+        // Start CSV output
+        PrintHeader();
+        
+        // Handle events
+        evm_.MainLoop();
+    }
+    
+private:
+
+    void LoadSampleRate()
+    {
+        SetSampleRate(GetSampleRate());
+    }
+
+    void SetSampleRate(uint32_t sampleRateMs)
+    {
+        pc_.SAMPLE_RATE_MS = sampleRateMs;
+        
+        eaPc_.Write(pc_);
+    }
+    
+    uint32_t GetSampleRate()
+    {
+        uint32_t retVal = DEFAULT_SAMPLE_RATE_MS;
+        
+        if (eaPc_.Read(pc_))
+        {
+            retVal = pc_.SAMPLE_RATE_MS;
+        }
+        
+        return retVal;
+    }
+    
+    void PrintHeader()
+    {
+        // print header
+        const char *sep = "";
+        for (uint8_t i = 0; i < 16; ++i)
+        {
+            Serial.print(sep);
+            Serial.print("CH"); Serial.print(i);
+            
+            sep = ",";
+        }
+        Serial.println();
+    }
+
+    void OnSample()
+    {
+        // print data
+        const char *sep = "";
+        for (uint8_t i = 0; i < 16; ++i)
+        {
+            uint16_t adcVal = adcMux_.AnalogReadChannel(i);
+            
+            Serial.print(sep);
+            Serial.print(adcVal);
+            
+            sep = ",";
+        }
+        Serial.println();
+    }
+
+    void SetUpUserInputHandlers()
+    {
+        console_.RegisterCommand("sampleRateMs", [this](char *cmdStr) {
+            Str str(cmdStr);
+            
+            if (str.TokenCount(' ') == 2)
+            {
+                uint32_t sampleRateMs = atol(str.TokenAtIdx(1, ' '));
+                
+                Serial.print("OK");
+                
+                SetSampleRate(sampleRateMs);
+                tedSample_.RegisterForTimedEventInterval(GetSampleRate(), 0);
+            }
+        });
+        
+        console_.ShowStartupMessage(0);
+        
+        console_.Start();
+    }
+
+private:
+
+    Evm::Instance<C_IDLE, C_TIMED, C_INTER> evm_;
+
+    AppMultimeterConfig &cfg_;
+    
+    PersistentConfiguration                   pc_;
+    EepromAccessor<PersistentConfiguration> eaPc_;
+    
+    TimedEventHandlerDelegate tedSample_;
+    
+    MuxAnalogDigitalCD74HC4067 adcMux_;
+    
+    static const uint8_t NUM_COMMANDS = 1;
+    SerialAsyncConsoleEnhanced<NUM_COMMANDS> console_;
+};
+
+
+#endif  // __APP_MULTIMETER_H__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sketches/AppMultimeterConfig/AppMultimeterConfig.ino
@@ -0,0 +1,24 @@
+#include "AppMultimeter.h"
+
+
+static AppMultimeterConfig cfg =
+{
+    // Pins used for multiplexer to indicate channel
+    .pinMuxBit0 = 11,
+    .pinMuxBit1 = 12,
+    .pinMuxBit2 = 13,
+    .pinMuxBit3 = 14,
+
+    // Pin doing actual ADC
+    .pinAdc = 25,
+};
+
+static AppMultimeter app(cfg);
+
+
+void setup()
+{
+    app.Run();
+}
+
+void loop() {}
