99bd3f2c1aadb50a56b7694346f0f1f1a2a829fc
dmalnati <douglas.malnati@gmail.com>
2018-03-26 21:40:11 -0400

    AVR TestEvmLowPowerPingPong added for programatic testing of delays

---
+++ sketches/TestEvmLowPowerPingPong/TestEvmLowPowerPingPong.ino
@@ -0,0 +1,51 @@
+#include "PAL.h"
+#include "Evm.h"
+#include "UtlSerial.h"
+
+
+static Evm::Instance<10,10,10> evm;
+static TimedEventHandlerDelegate ted;
+static SerialAsyncConsoleEnhanced<10>  console;
+
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting");
+    PAL.Delay(20);
+
+    console.RegisterCommand("ping", [](char *cmdStr){
+        Str str(cmdStr);
+        
+        if (str.TokenCount(' ') == 2)
+        {
+            uint32_t delayMs  = atol(str.TokenAtIdx(1, ' '));
+            Serial.print("ok ");
+            Serial.println(delayMs);
+            PAL.Delay(50);
+
+            ted.SetCallback([delayMs](){
+                Serial.print("pong ");
+                Serial.println(delayMs);
+                PAL.Delay(50);
+
+                console.Start();
+                evm.LowPowerDisable();
+            });
+            ted.RegisterForTimedEvent(delayMs);
+
+            console.Stop();
+            evm.LowPowerEnable();
+        }
+    });
+
+    console.SetVerbose(0);
+    console.Start();
+    evm.LowPowerDisable();
+
+    evm.MainLoopLowPower();
+}
+
+void loop() {}
+
+
