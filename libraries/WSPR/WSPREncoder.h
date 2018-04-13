d8c7074905eb02871ca425f1ac5035ea23e88986
dmalnati <douglas.malnati@gmail.com>
2018-04-13 00:01:33 -0400

    AVR WSPR lib initial checkin, derrived from VandeWettering genwspr

---
+++ libraries/WSPR/WSPREncoder.h
@@ -0,0 +1,201 @@
+#ifndef __WSPR_ENCODER_H__
+#define __WSPR_ENCODER_H__
+
+
+/*
+ * Here is some C code to generate "Type 1" WSPR messages, that
+ * include a single common callsign (no prefixes) and 4 character
+ * Maidenhead gridsquare location, as well as transmitted power.
+ * Some attempt has been made to minimize the storage used, so that
+ * this can compactly be used on small microcontrollers.
+ * 
+ * Very little error checking is done on this, so you better make sure
+ * that the callsign and gridsquare are of the appropriate form.
+ *
+ * Callsigns must be 2x3, 1x3, 2x1, or 1x2 for the purposes of this 
+ * code.
+ *
+ * Written by Mark VandeWettering K6HX
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
+class WSPREncoder
+{
+public:
+
+
+static int 
+chval1(int ch)
+{
+    if (isdigit(ch)) return ch - '0' ;
+    if (isalpha(ch)) return 10 + toupper(ch) - 'A' ;
+    if (ch == ' ') return 36 ;
+}
+
+static int 
+chval2(int ch)
+{
+    if (isalpha(ch)) return toupper(ch) - 'A' ;
+    if (ch == ' ') return 26 ;
+}
+
+
+static int
+encodecallsign(const char *callsign)
+{
+    /* find the first digit... */
+    int i, rc ;
+    char call[6] ;
+
+    for (i=0; i<6; i++) call[i] = ' ' ;
+
+    if (isdigit(callsign[1])) {
+	/* 1x callsigns... */
+	for (i=0; i<strlen(callsign); i++)
+	   call[1+i] = callsign[i] ;
+    } else if (isdigit(callsign[2])) {
+	/* 2x callsigns... */
+	for (i=0; i<strlen(callsign); i++)
+	   call[i] = callsign[i] ;
+    } else {
+	return 0 ;
+    }
+
+    rc  = chval1(call[0]) ; rc *= 36 ; 
+    rc += chval1(call[1]) ; rc *= 10 ;
+    rc += chval1(call[2]) ; rc *= 27 ;
+    rc += chval2(call[3]) ; rc *= 27 ;
+    rc += chval2(call[4]) ; rc *= 27 ;
+    rc += chval2(call[5]) ;
+
+    return rc ;
+}
+
+static int 
+encodegrid(const char *grid)
+{
+    int rc ;
+
+    rc = (179 - 10 * (grid[0]-'A') - (grid[2] - '0')) * 180
+	 + (10 * (grid[1]-'A')) + (grid[3] - '0') ;
+
+    return rc ;
+}
+
+static int
+encodepower(int p)
+{
+    return p + 64 ;
+}
+
+static int 
+parity(unsigned int x)
+{
+    unsigned int sx = x ;
+    int even = 0 ;
+    while (x) {
+	even = 1-even ;
+	x = x & (x - 1) ;
+    }
+    return even ;
+}
+
+static void
+genmsg(const char *call, const char *grid, const int power)
+{
+    int c = encodecallsign(call) ;
+    int g = encodegrid(grid) ;
+    int p = encodepower(power) ;
+    int i, mp = 0 ;
+    unsigned int acc = 0;
+
+    for (i=0; i<162; i++)
+	msg[i] = sync[i] ;
+
+    for (i=27; i>=0; i--) {		/* encode the callsign, 28 bits */
+	acc <<= 1 ;
+	if (c & (1<<i)) acc |= 1 ;
+	msg[rdx[mp++]] += 2*parity(acc & 0xf2d05351L) ;
+	msg[rdx[mp++]] += 2*parity(acc & 0xe4613c47L) ;
+    }
+
+    for (i=14; i>=0; i--) {		/* encode the grid, 15 bits */
+	acc <<= 1 ;
+	if (g & (1<<i)) acc |= 1 ;
+	msg[rdx[mp++]] += 2*parity(acc & 0xf2d05351L) ;
+	msg[rdx[mp++]] += 2*parity(acc & 0xe4613c47L) ;
+    }
+
+    for (i=6; i>=0; i--) {		/* encode the power, 7 bits */
+	acc <<= 1 ;
+	if (p & (1<<i)) acc |= 1 ;
+	msg[rdx[mp++]] += 2*parity(acc & 0xf2d05351L) ;
+	msg[rdx[mp++]] += 2*parity(acc & 0xe4613c47L) ;
+    }
+
+    for (i=30; i>=0; i--) {		/* pad with 31 zero bits */
+	acc <<= 1 ;
+	msg[rdx[mp++]] += 2*parity(acc & 0xf2d05351L) ;
+	msg[rdx[mp++]] += 2*parity(acc & 0xe4613c47L) ;
+    }
+}
+
+static const unsigned char sync[162];
+
+static const unsigned char rdx[162];
+
+static char msg[162];
+};
+
+const unsigned char WSPREncoder::sync[162] = {
+    1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0,
+    1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1,
+    0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0,
+    1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1,
+    1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+    0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1,
+    1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0
+} ;
+
+const unsigned char WSPREncoder::rdx[162] = {
+    0, 128, 64, 32, 160, 96, 16, 144, 80, 48, 112, 8, 136, 72, 40, 104, 24,
+    152, 88, 56, 120, 4, 132, 68, 36, 100, 20, 148, 84, 52, 116, 12, 140,
+    76, 44, 108, 28, 156, 92, 60, 124, 2, 130, 66, 34, 98, 18, 146, 82, 50,
+    114, 10, 138, 74, 42, 106, 26, 154, 90, 58, 122, 6, 134, 70, 38, 102,
+    22, 150, 86, 54, 118, 14, 142, 78, 46, 110, 30, 158, 94, 62, 126, 1,
+    129, 65, 33, 161, 97, 17, 145, 81, 49, 113, 9, 137, 73, 41, 105, 25,
+    153, 89, 57, 121, 5, 133, 69, 37, 101, 21, 149, 85, 53, 117, 13, 141,
+    77, 45, 109, 29, 157, 93, 61, 125, 3, 131, 67, 35, 99, 19, 147, 83, 51,
+    115, 11, 139, 75, 43, 107, 27, 155, 91, 59, 123, 7, 135, 71, 39, 103,
+    23, 151, 87, 55, 119, 15, 143, 79, 47, 111, 31, 159, 95, 63, 127 
+} ;
+
+char WSPREncoder::msg[162];
+
+
+
+#endif  // __WSPR_ENCODER_H__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sketches/TestWSPREncoder/TestWSPREncoder.ino
@@ -0,0 +1,104 @@
+#include <si5351.h>
+#include "WSPREncoder.h"
+#include "UtlStreamBlob.h"
+
+
+
+#define BUTTON                  12
+#define LED_PIN                 13
+
+
+Si5351 si5351;
+WSPREncoder wsprEncoder;
+
+
+void Transmit()
+{
+
+#define WSPR_DEFAULT_FREQ       14097200UL
+#define WSPR_TONE_SPACING       146           // ~1.46 Hz
+#define WSPR_DELAY              683          // Delay value for WSPR
+#define WSPR_SYMBOL_COUNT                   162
+
+
+    
+    uint32_t freq = WSPR_DEFAULT_FREQ;
+    uint8_t  symbol_count = WSPR_SYMBOL_COUNT; // From the library defines
+    uint16_t tone_spacing = WSPR_TONE_SPACING;
+    uint16_t tone_delay = WSPR_DELAY;
+    
+  si5351.output_enable(SI5351_CLK0, 1);
+
+  for(uint8_t i = 0; i < symbol_count; i++)
+  {
+      //si5351.set_freq((freq * 100) + (tx_buffer[i] * tone_spacing), SI5351_CLK0);
+      si5351.set_freq((freq * 100) + (wsprEncoder.msg[i] * tone_spacing), SI5351_CLK0);
+      delay(tone_delay);
+  }
+
+  si5351.output_enable(SI5351_CLK0, 0);
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting");
+    
+  // Initialize the Si5351
+  // Change the 2nd parameter in init if using a ref osc other
+  // than 25 MHz
+  si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
+
+  // Use a button connected to pin 12 as a transmit trigger
+  pinMode(BUTTON, INPUT_PULLUP);
+
+  // Set CLK0 output
+  si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_8MA); // Set for max power if desired
+  si5351.output_enable(SI5351_CLK0, 0); // Disable the clock initially
+
+  // Encode the message in the transmit buffer
+
+    //char message[] = "N0CALL AA00";
+    char call[] = "N0CALL";
+    char loc[] = "AA00";
+    uint8_t dbm = 27;
+
+  wsprEncoder.genmsg(call, loc, dbm);
+
+  StreamBlob(Serial, wsprEncoder.msg, 162, 1, 1);
+  Serial.println();
+}
+
+void loop()
+{
+  // Debounce the button and trigger TX on push
+  if(digitalRead(BUTTON) == LOW)
+  {
+    delay(50);   // delay to debounce
+    if (digitalRead(BUTTON) == LOW)
+    {
+        Serial.println("Encoding");
+        
+      Transmit();
+      delay(50); //delay to avoid extra triggers
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
