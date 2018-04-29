890a6b8d745e931c725ef7ffed0ae50dde991431
dmalnati <douglas.malnati@gmail.com>
2018-04-29 00:04:25 -0400

    AVR Log initial checkin

---
+++ libraries/PAL/Log.h
@@ -0,0 +1,248 @@
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#include <float.h>
+
+#include "Serial.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Log Init
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void LogStart(uint32_t baud)
+{
+    S0.Start(baud);
+}
+
+void LogEnd()
+{
+    S0.Stop();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Basic
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void LogNL()
+{
+    S0.Write('\n');
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Strings
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void LogNNL(const char *str)
+{
+    S0.Write((uint8_t *)str, strlen(str));
+}
+
+void Log(const char *str)
+{
+    LogNNL(str);
+    LogNL();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Chars
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void LogNNL(char val)
+{
+    S0.Write(val);
+}
+
+void Log(char val)
+{
+    LogNNL(val);
+    LogNL();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Unsigned Ints
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void LogNNL(uint32_t val)
+{
+    char buf[15];
+    ultoa(val, buf, 10);
+    LogNNL(buf);
+}
+
+void Log(uint32_t val)
+{
+    LogNNL(val);
+    LogNL();
+}
+
+void LogNNL(uint16_t val)
+{
+    LogNNL((uint32_t)val);
+}
+
+void Log(uint16_t val)
+{
+    Log((uint32_t)val);
+}
+
+void LogNNL(uint8_t val)
+{
+    LogNNL((uint32_t)val);
+}
+
+void Log(uint8_t val)
+{
+    Log((uint32_t)val);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Signed Ints
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void LogNNL(int32_t val)
+{
+    char buf[15];
+    ltoa(val, buf, 10);
+    LogNNL(buf);
+}
+
+void Log(int32_t val)
+{
+    LogNNL(val);
+    LogNL();
+}
+
+void LogNNL(int16_t val)
+{
+    LogNNL((int32_t)val);
+}
+
+void Log(int16_t val)
+{
+    Log((int32_t)val);
+}
+
+void LogNNL(int8_t val)
+{
+    LogNNL((int32_t)val);
+}
+
+void Log(int8_t val)
+{
+    Log((int32_t)val);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Floating Point
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void LogNNL(double val)
+{
+    // Support 3 decimal places
+    
+    // this is -MAX_DBL, which is the largest printed number I can find:
+    // 00000000011111111112222222222333333333344444
+    // 12345678901234567890123456789012345678901234
+    // -340282350000000000000000000000000000000.000
+    
+    char buf[45];
+    dtostrf(val, -1, 3, buf);
+    LogNNL(buf);
+}
+
+void Log(double val)
+{
+    LogNNL(val);
+    LogNL();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Templates
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void Log()
+{
+    // Nothing to do, simply here to make the templating work
+}
+
+void LogNNL()
+{
+    // Nothing to do, simply here to make the templating work
+}
+
+template <typename T1, typename T2, typename ...Ts>
+void LogNNL(T1 &&val1, T2 &&val2, Ts &&...args)
+{
+    LogNNL(val1);
+    LogNNL(val2);
+    LogNNL(args...);
+}
+
+template <typename T1, typename T2, typename ...Ts>
+void Log(T1 &&val1, T2 &&val2, Ts &&...args)
+{
+    LogNNL(val1, val2, args...);
+    LogNL();
+}
+
+template <typename T>
+void LogXNNL(T &&val, uint8_t count)
+{
+    for (uint8_t i = 0; i < count; ++i)
+    {
+        LogNNL(val);
+    }
+}
+
+template <typename T>
+void LogX(T &&val, uint8_t count)
+{
+    LogXNNL(val, count);
+    LogNL();
+}
+
+
+
+
+#endif  // __LOG_H__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sketches/TestLog/TestLog.ino
@@ -0,0 +1,134 @@
+#include "PAL.h"
+#include "Log.h"
+
+
+
+void TestStrings()
+{
+    Log("Strings");
+    LogX('-', 25);
+    
+    Log("heynow");
+    LogNNL("hey"); LogNNL("NOW"); LogNL();
+
+    LogNL();
+}
+
+void TestChars()
+{
+    Log("Chars");
+    LogX('-', 25);
+    
+    char c = '!';
+    Log(c);
+    LogNNL(c); LogNNL(c); LogNL();
+    
+    LogNL();
+}
+
+void TestUnsignedInts()
+{
+    Log("Unsigned Ints");
+    LogX('-', 25);
+    
+    Log((uint32_t) 0xDEADBEEF);
+    LogNNL((uint32_t) 0xDEADBEEF); LogNNL((uint32_t) 0xDEADBEEF); LogNL();
+
+    Log((uint16_t) 0xDEADBEEF);
+    LogNNL((uint16_t) 0xDEADBEEF); LogNNL((uint16_t) 0xDEADBEEF); LogNL();
+
+    Log((uint8_t) 0xDEADBEEF);
+    LogNNL((uint8_t) 0xDEADBEEF); LogNNL((uint8_t) 0xDEADBEEF); LogNL();
+
+    LogNL();
+}
+
+void TestSignedInts()
+{
+    Log("Signed Ints");
+    LogX('-', 25);
+    
+    Log((int32_t) 0xDEADBEEF);
+    LogNNL((int32_t) 0xDEADBEEF); LogNNL((int32_t) 0xDEADBEEF); LogNL();
+
+    Log((int16_t) 0xDEADBEEF);
+    LogNNL((int16_t) 0xDEADBEEF); LogNNL((int16_t) 0xDEADBEEF); LogNL();
+
+    Log((int8_t) 0xDEADBEEF);
+    LogNNL((int8_t) 0xDEADBEEF); LogNNL((int8_t) 0xDEADBEEF); LogNL();
+
+    LogNL();
+}
+
+void TestFloatingPoint()
+{
+    Log("Floating Point");
+    LogX('-', 25);
+
+
+    Log((float)-1.0);
+    Log((float)-0.0);
+    Log((float)0.0);
+    Log((float)1.0);
+
+    Log((float)-999999999.99999);
+    Log((float)999999999.99999);
+    
+    Log((float)-DBL_MAX);
+    Log((float)DBL_MAX);
+
+    
+    Log((double)-1.0);
+    Log((double)-0.0);
+    Log((double)0.0);
+    Log((double)1.0);
+
+    Log((double)-999999999.99999);
+    Log((double)999999999.99999);
+    
+    Log((double)-DBL_MAX);
+    Log((double)DBL_MAX);
+
+
+    LogNL();
+}
+
+void TestTemplates()
+{
+    Log("Templates");
+    LogX('-', 25);
+    
+    Log();
+    LogNNL();
+
+    Log("name = ", "test");
+    Log("value = ", 15);
+
+    Log("one = ", 1, " two = ", '2');
+
+    LogNL();
+}
+
+void setup()
+{
+    LogStart(9600);
+
+    while (1)
+    {
+        TestStrings();
+        TestChars();
+        TestUnsignedInts();
+        TestSignedInts();
+        TestFloatingPoint();
+        TestTemplates();
+
+        LogNL();
+
+        PAL.Delay(10000);
+    }
+}
+
+void loop()
+{
+    
+}
