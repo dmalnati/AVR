09ca791da6a4deb36517022a2e68d62f170f81e4
dmalnati <douglas.malnati@gmail.com>
2018-05-07 00:55:15 -0400

    ElectronicLoadR2 updated with notes on next changes

--- sketches/ElectronicLoadR2/measure.cpp
+++ sketches/ElectronicLoadR2/measure.cpp
@@ -33,6 +33,20 @@ void MeasureCal(const TMeasureCal *cal)
  */
 void MeasureGet(unsigned long *time, float *current, float *voltage)
 {
+    /*
+     * Enhancements
+     * - make time not reset after some pathetically short duration (4290.0 + 5)
+     *   - should time reset when you reset state also?
+     * - make new interval values take effect immediately
+     * - display limits when you do status?
+     *   - or change status concept to be 'measure' or something, and then have
+     *     some kind of "show my my state" command
+     *   - or even just show you when 'limit' run and no new value specified
+     * - give more space to the mAh column (maybe others too)
+     *   - will have to shift over when over 99mAh, which will happen often
+     * - anything come to mind to make scripting easier?
+     * - useful info in prompt?
+     */
     *time = micros();
     *current = calibration.cal_i * 5.0 * analogRead(PIN_MEASURE_A) / 4096;
     *voltage = calibration.cal_v * 19.8 * analogRead(PIN_MEASURE_V) / 4096;
