d42e68a148d56e8d819925dd80c4e99442818e53
U-DUSTY\doug <douglas.malnati@gmail.com>
2018-10-03 02:31:44 -0400

    AVR LedMatrixTimeMultiplexer initial checkin

---
+++ libraries/LED/LedMatrixTimeMultiplexer.h
@@ -0,0 +1,166 @@
+#ifndef __LED_MATRIX_TIME_MULTIPLEXER_H__
+#define __LED_MATRIX_TIME_MULTIPLEXER_H__
+
+
+#include "PAL.h"
+
+
+template <uint8_t ROW_COUNT, uint8_t COL_COUNT>
+class LedMatrixTimeMultiplexer
+{
+private:
+
+    static const uint32_t DEFAULT_ROW_INTERVAL_MS = 1;
+
+    struct LedData
+    {
+        uint8_t onOff = 0;
+    };
+
+
+public:
+
+    LedMatrixTimeMultiplexer(const uint8_t (&& rowPinList)[ROW_COUNT],
+                             const uint8_t (&& colPinList)[COL_COUNT])
+    : LedMatrixTimeMultiplexer(rowPinList, colPinList)
+    {
+        // Nothing to do
+    }
+    
+    LedMatrixTimeMultiplexer(const uint8_t (& rowPinList)[ROW_COUNT],
+                             const uint8_t (& colPinList)[COL_COUNT])
+    {
+        // Store, set all pins as output, set low
+        for (uint8_t row = 0; row < ROW_COUNT; ++row)
+        {
+            rowPinList_[row] = rowPinList[row];
+            
+            PAL.PinMode(rowPinList_[row], OUTPUT);
+            PAL.DigitalWrite(rowPinList_[row], LOW);
+        }
+        
+        for (uint8_t col = 0; col < COL_COUNT; ++col)
+        {
+            colPinList_[col] = colPinList[col];
+            
+            PAL.PinMode(colPinList_[col], OUTPUT);
+            PAL.DigitalWrite(colPinList_[col], LOW);
+        }
+    }
+    
+    void SetLedState(uint8_t row, uint8_t col, uint8_t onOff)
+    {
+        if (row < ROW_COUNT && col < COL_COUNT)
+        {
+            rowByColMatrix_[row][col].onOff = onOff;
+        }
+    }
+    
+    void SetRowIntervalMs(uint32_t rowIntervalMs)
+    {
+        rowIntervalMs_ = rowIntervalMs;
+    }
+
+    void Start()
+    {
+        Stop();
+        
+        ted_.SetCallback([this](){
+            RenderNextRow();
+        });
+        
+        ted_.RegisterForTimedEventInterval(rowIntervalMs_);
+    }
+    
+    void Stop()
+    {
+        RowClear(rowIdx_);
+        
+        rowIdx_ = 0;
+        
+        ted_.DeRegisterForTimedEvent();
+    }
+
+
+private:
+    
+    void RenderNextRow()
+    {
+        RowClear(rowIdx_);
+        
+        rowIdx_ = (rowIdx_ + 1) % ROW_COUNT;
+        
+        RowPaint(rowIdx_);
+    }
+    
+    void RowPaint(uint8_t rowIdx)
+    {
+        // Activate the row
+        PAL.DigitalWrite(rowPinList_[rowIdx], HIGH);
+        
+        for (uint8_t col = 0; col < COL_COUNT; ++col)
+        {
+            LedData &ledData = rowByColMatrix_[rowIdx][col];
+            
+            // Activate the column if the LED is set on
+            if (ledData.onOff)
+            {
+                PAL.DigitalWrite(colPinList_[col], HIGH);
+            }
+        }
+    }
+    
+    void RowClear(uint8_t rowIdx)
+    {
+        // Deactivate the row
+        PAL.DigitalWrite(rowPinList_[rowIdx], LOW);
+        
+        // Unconditionally put all low.
+        // Deals with scenario where between RowPaint and RowClear, the current
+        // row is modified by calling code and therefore we would lose the
+        // ability to see what's set by looking at stateful data.
+        // Here no state is required.
+        for (uint8_t col = 0; col < COL_COUNT; ++col)
+        {
+            PAL.DigitalWrite(colPinList_[col], LOW);
+        }
+    }
+
+
+
+    uint8_t rowPinList_[ROW_COUNT] = { 0 };
+    uint8_t colPinList_[COL_COUNT] = { 0 };
+
+    LedData rowByColMatrix_[ROW_COUNT][COL_COUNT];
+    
+    uint8_t rowIdx_ = 0;
+    
+    uint32_t                   rowIntervalMs_ = DEFAULT_ROW_INTERVAL_MS;
+    TimedEventHandlerDelegate  ted_;
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif  // __LED_MATRIX_TIME_MULTIPLEXER_H__
---
+++ sketches/TestLedMatrixTimeMultiplexer/TestLedMatrixTimeMultiplexer.ino
@@ -0,0 +1,113 @@
+#include "Evm.h"
+#include "SerialInput.h"
+#include "LedMatrixTimeMultiplexer.h"
+
+
+static Evm::Instance<10,10,10> evm;
+static SerialAsyncConsoleEnhanced<10> shell;
+
+static const uint8_t ROW_COUNT = 3;
+static const uint8_t COL_COUNT = 2;
+static const uint8_t ROW_PIN_LIST[ROW_COUNT] = { 19, 18, 17 };
+static const uint8_t COL_PIN_LIST[COL_COUNT] = { 16, 15 };
+static LedMatrixTimeMultiplexer<ROW_COUNT, COL_COUNT> mux(ROW_PIN_LIST, COL_PIN_LIST);
+
+
+void setup()
+{
+  LogStart(9600);
+  Log(P("Starting"));
+        
+            
+  // Set up interactive commands
+  
+  shell.RegisterCommand("set", [](char *cmdStr){
+      Str str(cmdStr);
+  
+      if (str.TokenCount(' ') == 4)
+      {
+          uint8_t row   = atoi(str.TokenAtIdx(1, ' '));
+          uint8_t col   = atoi(str.TokenAtIdx(2, ' '));
+          uint8_t onOff = atoi(str.TokenAtIdx(3, ' '));
+          
+          Log("Setting: ", row, ", ", col, ", ", onOff);
+          
+          mux.SetLedState(row, col, onOff);
+      }
+  });
+
+#if 0
+  shell.RegisterCommand("p", [](char *cmdStr){
+      Str str(cmdStr);
+  
+      if (str.TokenCount(' ') == 2)
+      {
+          uint8_t row   = atoi(str.TokenAtIdx(1, ' '));
+          
+          Log("Painting row: ", row);
+          
+          mux.RowPaint(row);
+      }
+  });
+
+  shell.RegisterCommand("c", [](char *cmdStr){
+      Str str(cmdStr);
+  
+      if (str.TokenCount(' ') == 2)
+      {
+          uint8_t row   = atoi(str.TokenAtIdx(1, ' '));
+          
+          Log("Clearing row: ", row);
+          
+          mux.RowClear(row);
+      }
+  });
+#endif
+
+  shell.RegisterCommand("start", [](char *){
+      Log("Starting");
+      mux.Start();
+  });
+
+  shell.RegisterCommand("stop", [](char *){
+      Log("Stopping");
+      mux.Stop();
+  });
+
+
+  // Thoughts on interval timing
+  //
+  // 60 frames/sec is a good target
+  // 1000 / 60 = 16.66ms
+  // So if I have 16.66ms (call it 16) to zip through all the rows, that
+  // time budget for each is 16 / ROW_COUNT
+  //
+  // if ROW_COUNT < 16, then at least 1ms per row
+  // if ROW_COUNT == 16, 1ms per row
+  // if ROW_COUNT > 16, then 60 frames/sec isn't going to work well but
+  //     we'll try and just shoot for 1ms per row
+  shell.RegisterCommand("ms", [](char *cmdStr){
+      Str str(cmdStr);
+  
+      if (str.TokenCount(' ') == 2)
+      {
+          uint32_t ms = atol(str.TokenAtIdx(1, ' '));
+          
+          Log("Starting at ms = ", ms);
+          mux.SetRowIntervalMs(ms);
+          mux.Start();
+      }
+  });
+
+  shell.Start();
+
+  Log("Running");
+
+  evm.MainLoop();
+}
+
+void loop(){}
+
+
+
+
