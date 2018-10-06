35198aaee4b8ea45b64351c3b988533accbdfd6b
U-DUSTY\doug <douglas.malnati@gmail.com>
2018-10-06 12:02:00 -0400

    AVR AppGateRemote working, now interactive

--- sketches/AppGateRemote/AppGateRemote.ino
+++ sketches/AppGateRemote/AppGateRemote.ino
@@ -1,4 +1,8 @@
 #include "PAL.h"
+#include "Log.h"
+#include "SerialInput.h"
+#include "Evm.h"
+#include "PinInput.h"
 #include "RFSI4463PRO.h"
 
 
@@ -36,9 +40,16 @@ low  duration plus gap:  .485ms + 1.445ms = 1.93ms
 
 total duration should then be: 10 x 1.92ms = 19.2ms (confirmed)
 
+
+20ms delay between codes on the real remote.
+
  */
 
 
+
+static Evm::Instance<10,10,10> evm;
+static SerialAsyncConsoleEnhanced<20> shell;
+static TimedEventHandlerDelegate ted;
 
 static const uint8_t PIN_SS       = 25;
 static const uint8_t PIN_SHUTDOWN = 14;
@@ -46,11 +57,12 @@ static const uint8_t PIN_OOK      =  5;
 
 static RFSI4463PRO rf(PIN_SS, PIN_SHUTDOWN);
 
+// drive this pin low first, otherwise it floats and accidentally
+// gives input to the RF module and you see transmissions
+static Pin pinOOK(PIN_OOK, LOW);
 
 void SendCombination(uint16_t combo10bit)
 {
-    Pin pinOOK(PIN_OOK, LOW);
-
     uint16_t bitShiftBuf = combo10bit;
     uint8_t  bitSentCount = 0;
 
@@ -78,25 +90,99 @@ void SendCombination(uint16_t combo10bit)
     }
 }
 
+static uint32_t betw = 20;
+
+void SendCombinationMulti(uint16_t combo10bit, uint16_t count)
+{
+    for (uint8_t i = 0; i < count; ++i)
+    {
+        SendCombination(combo10bit);
+        PAL.Delay(betw);
+    }
+}
+
 void setup()
 {
-    @fix@Serial.begin(9600);
-    @fix@Serial.println("Starting");
+    LogStart(9600);
+    Log("Starting");
 
     rf.Init();
     const uint32_t FREQ = 310000000;
     rf.SetFrequency(FREQ);
-    rf.SetModemAsyncOOK();
-    rf.StartTx();
+    rf.SetModulationType(RFSI4463PRO::ModulationType::MT_OOK);
+    rf.Start();
 
     const uint16_t COMBO = 0b0000001110000111;
 
-    while (1)
-    {
-        SendCombination(COMBO);
 
-        PAL.Delay(1000);
-    }
+    static const uint32_t BIT_PATTERN_WARMUP = 0b10101010101010;
+
+    static uint8_t warmSendCount = 3;
+    static uint8_t codeSendCount = 1;
+    static uint8_t postSendCount = 0;
+
+    shell.RegisterCommand("warm", [&](char *cmdStr){
+        Str str(cmdStr);
+  
+        if (str.TokenCount(' ') == 2)
+        {
+            warmSendCount = atoi(str.TokenAtIdx(1, ' '));
+
+            Log("WarmSendCount = ", warmSendCount);
+        }
+    });
+
+    shell.RegisterCommand("code", [&](char *cmdStr){
+        Str str(cmdStr);
+  
+        if (str.TokenCount(' ') == 2)
+        {
+            codeSendCount = atoi(str.TokenAtIdx(1, ' '));
+
+            Log("CodeSendCount = ", codeSendCount);
+        }
+    });
+    
+    shell.RegisterCommand("post", [&](char *cmdStr){
+        Str str(cmdStr);
+  
+        if (str.TokenCount(' ') == 2)
+        {
+            postSendCount = atoi(str.TokenAtIdx(1, ' '));
+
+            Log("postSendCount = ", postSendCount);
+        }
+    });
+
+    shell.RegisterCommand("betw", [&](char *cmdStr){
+        Str str(cmdStr);
+  
+        if (str.TokenCount(' ') == 2)
+        {
+            betw = atoi(str.TokenAtIdx(1, ' '));
+
+            Log("betw = ", betw);
+        }
+    });
+
+    // Send
+    shell.RegisterCommand("send", [](char *){
+        // Warm up receiver
+        SendCombinationMulti(BIT_PATTERN_WARMUP, warmSendCount);
+
+        // Send code
+        SendCombinationMulti(COMBO, codeSendCount);
+        
+        // Send outro
+        SendCombinationMulti(BIT_PATTERN_WARMUP, postSendCount);
+    });
+
+    shell.Start();
+
+
+    
+    Log("Running");
+    evm.MainLoop();
 }
 
 
