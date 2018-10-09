592183361ec8ab28f641a6403b9b9d2a20889ad2
U-DUSTY\doug <douglas.malnati@gmail.com>
2018-10-08 23:28:22 -0400

    AVR TestPinInput (and Analog) update

---
+++ sketches/TestPinInput/TestPinInput.ino
@@ -0,0 +1,40 @@
+#include "PAL.h"
+#include "Log.h"
+#include "Evm.h"
+#include "PinInput.h"
+
+
+static uint8_t PIN_ANALOG_1 = 28;
+static uint8_t PIN_ANALOG_2 = 27;
+
+static Evm::Instance<10,10,10> evm;
+static PinInput pi1(PIN_ANALOG_1);
+static PinInput pi2(PIN_ANALOG_2);
+
+
+static void OnRead(uint8_t pin, uint16_t val)
+{
+    Log(PAL.Millis());
+    
+    Log(pin, ": ", val);
+    LogNL();
+}
+
+void setup()
+{
+    LogStart(9600);
+    Log("Starting");
+
+    pi1.SetCallback([=](uint16_t val) { OnRead(PIN_ANALOG_1, val); });
+    pi1.Enable();
+    pi2.SetCallback([=](uint16_t val) { OnRead(PIN_ANALOG_2, val); });
+    pi2.Enable();
+
+    Log("Running");
+    evm.MainLoop();
+}
+
+void loop() {}
+
+
+
--- sketches/TestPinInputAnalog/TestPinInputAnalog.ino
+++ sketches/TestPinInputAnalog/TestPinInputAnalog.ino
@@ -1,4 +1,5 @@
 #include "PAL.h"
+#include "Log.h"
 #include "Evm.h"
 #include "PinInputAnalog.h"
 
@@ -13,24 +14,23 @@ static PinInputAnalog pia2(PIN_ANALOG_2);
 
 static void OnAnalogRead(uint8_t pin, uint16_t val)
 {
-    @fix@Serial.println(PAL.Millis());
+    Log(PAL.Millis());
     
-    @fix@Serial.print(pin);
-    @fix@Serial.print(": ");
-    @fix@Serial.println(val);
-
-    @fix@Serial.println("");
+    Log(pin, ": ", val);
+    LogNL();
 }
 
 void setup()
 {
-    @fix@Serial.begin(9600);
+    LogStart(9600);
+    Log("Starting");
 
     pia1.SetCallback([=](uint16_t val) { OnAnalogRead(PIN_ANALOG_1, val); });
     pia1.Enable();
     pia2.SetCallback([=](uint16_t val) { OnAnalogRead(PIN_ANALOG_2, val); });
     pia2.Enable();
 
+    Log("Running");
     evm.MainLoop();
 }
 
