7a5809e137fb931fb8a2519951619603f9e8e012
U-DUSTY\doug <douglas.malnati@gmail.com>
2018-10-16 02:42:12 -0400

    AVR AppEvilEye mid-development checkin, LED basic animations working

--- libraries/AppEvilEye/AppEvilEye.h
+++ libraries/AppEvilEye/AppEvilEye.h
@@ -4,6 +4,9 @@
 
 #include "Log.h"
 #include "Evm.h"
+#include "FunctionChain.h"
+#include "SerialInput.h"
+#include "SensorCapacitiveTouchCAP1188.h"
 #include "LedMatrixTimeMultiplexer.h"
 
 
@@ -16,24 +19,26 @@ class AppEvilEye
 {
 public:
     
-    static const uint8_t C_IDLE  =  0;
+    static const uint8_t C_IDLE  = 20;
     static const uint8_t C_TIMED = 20;
-    static const uint8_t C_INTER =  0;
+    static const uint8_t C_INTER = 20;
     
     
 private:
     
-    static const uint8_t ROW_COUNT = 3;
-    static const uint8_t COL_COUNT = 2;
+    static const uint8_t ROW_COUNT = 14;
+    static const uint8_t COL_COUNT = 4;
     
+    static const uint8_t CAP_I2C_ADDR = 0x28;
 
     
 public:
     AppEvilEye(AppEvilEyeConfig &cfg)
     : cfg_(cfg)
-    , mux_(
-        { 19, 18, 17 },
-        { 16, 15 }
+    , cap_(CAP_I2C_ADDR)
+    , mux_(        
+        { 15, 14, 16, 17, 18, 19, 9, 10, 23, 24, 25, 26, 4, 5 },
+        { 6, 11, 12, 13 }
       )
     {
         // Nothing to do
@@ -52,10 +57,8 @@ public:
         Log(P("Starting"));
         
         
-        
-        
-        
-        
+        // Setup interactive controls
+        SetupShell();
         
         // Start up LEDs
         mux_.Start();
@@ -71,12 +74,159 @@ public:
     
 private:
 
+    void SetupShell()
+    {
+        shell_.RegisterCommand("start", [this](char *){
+            Log("Starting");
+            mux_.Start();
+        });
+
+        shell_.RegisterCommand("stop", [this](char *){
+            Log("Stopping");
+            mux_.Stop();
+        });
+        
+        shell_.RegisterCommand("on", [this](char *){
+            Log("On");
+            LedSetAllOn();
+        });
+        
+        shell_.RegisterCommand("off", [this](char *){
+            Log("Off");
+            LedSetAllOff();
+        });
+
+        shell_.RegisterCommand("us", [this](char *cmdStr){
+            Str str(cmdStr);
+
+            if (str.TokenCount(' ') == 2)
+            {
+                uint32_t us = atol(str.TokenAtIdx(1, ' '));
+
+                Log("us = ", us);
+                mux_.SetRowIntervalUs(us);
+            }
+        });
+        
+        shell_.RegisterCommand("rampup", [this](char *cmdStr){
+            Str str(cmdStr);
+
+            if (str.TokenCount(' ') == 2)
+            {
+                uint32_t ms = atol(str.TokenAtIdx(1, ' '));
+
+                Log("Ramp Up at ", ms, " ms");
+                
+                DoAnimationRampUp(ms);
+            }
+        });
+        
+        shell_.RegisterCommand("rampdn", [this](char *cmdStr){
+            Str str(cmdStr);
+
+            if (str.TokenCount(' ') == 2)
+            {
+                uint32_t ms = atol(str.TokenAtIdx(1, ' '));
+
+                Log("Ramp Down at ", ms, " ms");
+                
+                DoAnimationRampDown(ms);
+            }
+        });
+        
+        
+        shell_.Start();
+    }
+    
+    
+    void LedSetAll(uint8_t onOff)
+    {
+        for (uint8_t row = 0; row < ROW_COUNT; ++row)
+        {
+            for (uint8_t col = 0; col < COL_COUNT; ++col)
+            {
+                mux_.SetLedState(row, col, onOff);
+            }
+        }
+    }
+    
+    void LedSetAllOn()
+    {
+        LedSetAll(1);
+    }
+    
+    void LedSetAllOff()
+    {
+        LedSetAll(0);
+    }
+    
+    void DoAnimationRampUp(uint32_t intervalBetweenSpeedsMs)
+    {
+        LedSetAllOn();
+        
+        fnChain_.Reset();
+        
+        uint8_t delayMs = 0;
+        uint32_t rowIntervalUsList[] = { 50000, 40000, 30000, 20000, 10000, 5000, 2500, 1800, 500 };
+        for (auto rowIntervalUs : rowIntervalUsList)
+        {
+            fnChain_.Append([=](){
+                mux_.SetRowIntervalUs(rowIntervalUs);
+            }, delayMs);
+            
+            delayMs = intervalBetweenSpeedsMs;
+        }
+        
+        fnChain_.SetCallbackOnComplete([this](){ LedSetAllOff(); }, intervalBetweenSpeedsMs);
+
+        fnChain_.Start();
+    }
+    
+    void DoAnimationRampDown(uint32_t intervalBetweenSpeedsMs)
+    {
+        LedSetAllOn();
+        
+        fnChain_.Reset();
+        
+        uint8_t delayMs = 0;
+        uint32_t rowIntervalUsList[] = { 500, 1800, 2500, 5000, 10000, 20000, 30000, 40000, 50000 };
+        for (auto rowIntervalUs : rowIntervalUsList)
+        {
+            fnChain_.Append([=](){
+                mux_.SetRowIntervalUs(rowIntervalUs);
+            }, delayMs);
+            
+            delayMs = intervalBetweenSpeedsMs;
+        }
+        
+        fnChain_.SetCallbackOnComplete([this](){ LedSetAllOff(); }, intervalBetweenSpeedsMs);
+
+        fnChain_.Start();
+    }
+
+
+    
+    
+    
+    
+    
+    
+
     Evm::Instance<C_IDLE, C_TIMED, C_INTER> evm_;
 
     AppEvilEyeConfig &cfg_;
     
+    SensorCapacitiveTouchCAP1188 cap_;
     
     LedMatrixTimeMultiplexer<ROW_COUNT, COL_COUNT> mux_;
+    
+    static const uint8_t FN_CHAIN_LEN = 20;
+    FunctionChainAsync<FN_CHAIN_LEN> fnChain_;
+
+    
+    
+    static const uint8_t CONSOLE_CMD_COUNT = 20;
+    SerialAsyncConsoleEnhanced<CONSOLE_CMD_COUNT> shell_;
 
     
 };
