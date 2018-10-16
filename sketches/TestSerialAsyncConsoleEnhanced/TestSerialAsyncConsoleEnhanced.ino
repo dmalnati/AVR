5a9a382ebb72707cdaf5dc0ba21edc229dd0b59c
U-DUSTY\doug <douglas.malnati@gmail.com>
2018-10-16 02:39:08 -0400

    AVR TestSerialAsyncConsoleEnhanced

---
+++ sketches/TestSerialAsyncConsoleEnhanced/TestSerialAsyncConsoleEnhanced.ino
@@ -0,0 +1,27 @@
+#include "Evm.h"
+#include "Log.h"
+#include "SerialInput.h"
+
+
+static Evm::Instance<10,10,10> evm;
+static SerialAsyncConsoleEnhanced<10> shell;
+
+
+void setup()
+{
+    LogStart(9600);
+    Log("Starting");
+
+    shell.Start();
+
+    evm.MainLoop();
+}
+
+void loop() {}
+
+
+
+
+
+
+
