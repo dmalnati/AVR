dee2c3976a975ac0ef0b4ef37e5f2af8f1579129
U-DUSTY\doug <douglas.malnati@gmail.com>
2018-10-20 16:56:52 -0400

    AVR SensorCapacitiveTouchCAP1188 low power and polling callback

--- libraries/Peripheral/SensorCapacitiveTouchCAP1188.h
+++ libraries/Peripheral/SensorCapacitiveTouchCAP1188.h
@@ -4,6 +4,7 @@
 
 #include "PAL.h"
 #include "I2C.h"
+#include "TimedEventHandler.h"
 
 
 // http://ww1.microchip.com/downloads/en/DeviceDoc/CAP1188%20.pdf
@@ -16,6 +17,12 @@ class SensorCapacitiveTouchCAP1188
     static const uint8_t REG_SENSOR_INPUT_ENABLE          = 0x21;
     static const uint8_t REG_MULTIPLE_TOUCH_CONFIGURATION = 0x2A;
     static const uint8_t REG_SENSOR_INPUT_LED_LINKING     = 0x72;
+    
+    static const uint8_t REG_SENSOR_STANDBY_INPUT_ENABLE  = 0x40;
+    static const uint8_t REG_STANDBY_CONFIGURATION        = 0x41;
+    static const uint8_t REG_STANDBY_SENSITIVITY_CONTROL  = 0x42;
+    
+    static const uint32_t DEFAULT_POLL_PERIOD_MS = 100;
     
 public:
     SensorCapacitiveTouchCAP1188(uint8_t addr)
@@ -75,12 +82,97 @@ public:
     
     // Set which inputs are monitored by the chip
     // Input to this function is a bitmap corresponding to 01234567
-    // (this is despite the fact that the chip wants preferences specified in
-    //  reverse of that)
     void SetInputEnable(uint8_t bitmap)
     {
         uint8_t bitmapUse = PAL.BitReverse(bitmap);
         I2C.WriteRegister(addr_, REG_SENSOR_INPUT_ENABLE, bitmapUse);
+    }
+    
+    void PowerActive()
+    {
+        uint8_t regVal;
+        I2C.ReadRegister(addr_, REG_MAIN_CONTROL, regVal);
+        regVal &= ~0b00100000;
+        I2C.WriteRegister(addr_, REG_MAIN_CONTROL, regVal);
+    }
+    
+    void PowerStandby()
+    {
+        uint8_t regVal;
+        I2C.ReadRegister(addr_, REG_MAIN_CONTROL, regVal);
+        regVal |= 0b00100000;
+        I2C.WriteRegister(addr_, REG_MAIN_CONTROL, regVal);
+    }
+    
+    
+    enum class StandbyAvgOrSum : uint8_t
+    {
+        AVG = 0,  // default
+        SUM = 1,
+    };
+    
+    enum class StandbySamplesPerMeasurement : uint8_t
+    {
+        COUNT_1   = 0,
+        COUNT_2   = 1,
+        COUNT_4   = 2,
+        COUNT_8   = 3,  // default
+        COUNT_16  = 4,
+        COUNT_32  = 5,
+        COUNT_64  = 6,
+        COUNT_128 = 7,
+    };
+    
+    enum class StandbySampleTime : uint8_t
+    {
+        US_320  = 0,
+        US_640  = 1,
+        US_1280 = 2,  // default
+        US_2560 = 3,
+    };
+    
+    enum class StandbyCycleTime : uint8_t
+    {
+        MS_35  = 0,
+        MS_70  = 1,  // default
+        MS_105 = 2,
+        MS_140 = 3,
+    };
+    
+    void StandbyConfigure(StandbyAvgOrSum               avgOrSum,
+                          StandbySamplesPerMeasurement  samplesPerMeasurement,
+                          StandbySampleTime             sampleTime,
+                          StandbyCycleTime              cycleTime)
+    {
+        uint8_t regVal = 
+            (((uint8_t)avgOrSum) << 7)              |
+            (((uint8_t)samplesPerMeasurement) << 4) |
+            (((uint8_t)sampleTime) << 2)            |
+            ((uint8_t)cycleTime);
+        
+        I2C.WriteRegister(addr_, REG_STANDBY_CONFIGURATION, regVal);
+    }
+    
+    
+    
+    // Multiplying factor, 2^x
+    // Inputs range 0-7, for a result range of 1-128
+    // Higher the value, the more sensitive
+    void SetStandbySensitivity(uint8_t multFactor)
+    {
+        if (multFactor > 7)
+        {
+            multFactor = 7;
+        }
+        
+        I2C.WriteRegister(addr_, REG_STANDBY_SENSITIVITY_CONTROL, ~multFactor);
+    }
+    
+    
+    void SetStandbyInputEnable(uint8_t bitmap)
+    {
+        uint8_t bitmapUse = PAL.BitReverse(bitmap);
+        I2C.WriteRegister(addr_, REG_SENSOR_STANDBY_INPUT_ENABLE, bitmapUse);
     }
     
     uint8_t GetTouched()
@@ -88,7 +180,7 @@ public:
         // Get current value
         uint8_t touched = 0;
         I2C.ReadRegister(addr_, REG_SENSOR_INPUT_STATUS, touched);
-
+        
         // Tell sensor to clear non-current touches, so next read will
         // only show what is current (as opposed to what has been touched
         // ever since prior poll)
@@ -103,10 +195,52 @@ public:
         
         return touched;
     }
+    
+    void SetCallback(function<void(uint8_t bitmapTouched)> cbFn)
+    {
+        cbFn_ = cbFn;
+    }
+    
+    void SetPollPeriodMs(uint32_t pollPeriodMs)
+    {
+        pollPeriodMs_ = pollPeriodMs;
+    }
+    
+    void Start()
+    {
+        Stop();
+        
+        ted_.SetCallback([this](){
+            uint8_t bitmapTouched = GetTouched();
+            
+            if (bitmapTouched != bitmapTouchedLast_)
+            {
+                cbFn_(bitmapTouched);
+                
+                bitmapTouchedLast_ = bitmapTouched;
+            }
+        });
+        
+        ted_.RegisterForTimedEventInterval(pollPeriodMs_, 0);
+    }
+    
+    void Stop()
+    {
+        ted_.DeRegisterForTimedEvent();
+        
+        bitmapTouchedLast_ = 0;
+    }
 
 private:
 
     uint8_t addr_;
+    
+    function<void(uint8_t bitmapTouched)> cbFn_;
+    uint8_t bitmapTouchedLast_ = 0;
+    
+    TimedEventHandlerDelegate ted_;
+    
+    uint32_t pollPeriodMs_ = DEFAULT_POLL_PERIOD_MS;
 };
 
 
--- sketches/TestSensorCapacitiveTouchCAP1188/TestSensorCapacitiveTouchCAP1188.ino
+++ sketches/TestSensorCapacitiveTouchCAP1188/TestSensorCapacitiveTouchCAP1188.ino
@@ -1,55 +1,187 @@
-#include "PAL.h"
+#include "Evm.h"
+#include "TimedEventHandler.h"
+#include "SerialInput.h"
 #include "SensorCapacitiveTouchCAP1188.h"
 
 
-static const uint8_t ADDR = 0x28;
-//static const uint8_t ADDR = 0x2B;
+static Evm::Instance<20,20,20> evm;
+static SerialAsyncConsoleEnhanced<30> shell;
 
-SensorCapacitiveTouchCAP1188 cap(ADDR);
+static const uint8_t CAP_I2C_ADDR = 0x28;
+static SensorCapacitiveTouchCAP1188 cap(CAP_I2C_ADDR);
+
+static TimedEventHandlerDelegate ted;
 
 
 void setup()
 {
-    @fix@Serial.begin(9600);
-    @fix@Serial.println("Starting");
+    LogStart(9600);
+    Log(P("Starting"));
 
-    Pin dbg(14, LOW);
+    // Set up touch sensor
 
-    cap.EnableLEDs();
 
-    uint8_t touchedLast = 0;
     
-    while (1)
-    {
-        //@fix@Serial.println("Checking");
 
-        PAL.DigitalToggle(dbg);
-        uint8_t touched = cap.GetTouched();
-        PAL.DigitalToggle(dbg);
 
-        if (touched != touchedLast)
+    // Set up input
+    shell.RegisterCommand("sens", [](char *cmdStr){
+      Str str(cmdStr);
+    
+      if (str.TokenCount(' ') == 2)
+      {
+          uint8_t multFactor = atoi(str.TokenAtIdx(1, ' '));
+
+          Log("Mult Factor: ", multFactor);
+
+          cap.SetSensitivity(multFactor);
+      }
+    });
+    
+    shell.RegisterCommand("ssens", [](char *cmdStr){
+      Str str(cmdStr);
+    
+      if (str.TokenCount(' ') == 2)
+      {
+          uint8_t multFactor = atoi(str.TokenAtIdx(1, ' '));
+
+          Log("Standby Mult Factor: ", multFactor);
+
+          cap.SetStandbySensitivity(multFactor);
+      }
+    });
+
+    shell.RegisterCommand("enable", [](char *cmdStr){
+      Str str(cmdStr);
+    
+      if (str.TokenCount(' ') == 2)
+      {
+          uint8_t bitmap = atoi(str.TokenAtIdx(1, ' '));
+
+          Log("Enable Bitmap: ", LogBIN(bitmap));
+
+          cap.SetInputEnable(bitmap);
+      }
+    });
+
+    shell.RegisterCommand("senable", [](char *cmdStr){
+      Str str(cmdStr);
+    
+      if (str.TokenCount(' ') == 2)
+      {
+          uint8_t bitmap = atoi(str.TokenAtIdx(1, ' '));
+
+          Log("Standby Enable Bitmap: ", LogBIN(bitmap));
+
+          cap.SetStandbyInputEnable(bitmap);
+      }
+    });
+
+    
+
+    
+    shell.RegisterCommand("get", [](char *){
+        Log("Get");
+
+        uint8_t bitmapTouched = cap.GetTouched();
+        
+        Log("Got: ", LogBIN(bitmapTouched));
+    });
+
+    shell.RegisterCommand("start", [](char *){
+        Log("Start");
+
+        cap.SetCallback([](uint8_t bitmapTouched){
+            Log("Touched: ", LogBIN(bitmapTouched));
+        });
+        cap.Start();
+    });
+
+    shell.RegisterCommand("stop", [](char *){
+        Log("Stop");
+
+        cap.Stop();
+    });
+
+
+
+
+
+
+    shell.RegisterCommand("pactive", [](char *){
+        Log("Power: Active");
+
+        cap.PowerActive();
+    });
+
+    shell.RegisterCommand("pstandby", [](char *){
+        Log("Power: Standby");
+
+        cap.PowerStandby();
+    });
+
+
+
+/*
+# SUM, 1 sample, 1280us sample time, 140ms cycle
+# 9-26us, 0.5 inch range
+sconf 1 0 2 3
+
+# AVG, 1 sample, 1280us sample time, 140ms cycle
+# 9-26us, 0.5 inch range
+sconf 0 7 2 3
+
+# AVG, 1 sample, 2650us sample time, 140ms cycle
+# 15-32us, 1.0 inch range
+sconf 0 0 3 3
+
+# AVG, 1 sample, 320us sample time, 140ms cycle
+# 9-17us, 0 inch range
+# (change to SUM didn't help)
+sconf 0 0 0 3
+*/
+    shell.RegisterCommand("sconf", [](char *cmdStr){
+        Str str(cmdStr);
+        
+        Log("Standby Configure");
+
+        if (str.TokenCount(' ') == 5)
         {
-            for (uint8_t i = 0; i < 8; ++i)
-            {
-                if ((touched << i) & 0x80)
-                {
-                    @fix@Serial.print(i);
-                }
-                else
-                {
-                    @fix@Serial.print(" ");
-                }
-            }
-            @fix@Serial.println();
+            SensorCapacitiveTouchCAP1188::StandbyAvgOrSum               avgOrSum              = (SensorCapacitiveTouchCAP1188::StandbyAvgOrSum)atoi(str.TokenAtIdx(1, ' '));
+            SensorCapacitiveTouchCAP1188::StandbySamplesPerMeasurement  samplesPerMeasurement = (SensorCapacitiveTouchCAP1188::StandbySamplesPerMeasurement)atoi(str.TokenAtIdx(2, ' '));
+            SensorCapacitiveTouchCAP1188::StandbySampleTime             sampleTime            = (SensorCapacitiveTouchCAP1188::StandbySampleTime)atoi(str.TokenAtIdx(3, ' '));
+            SensorCapacitiveTouchCAP1188::StandbyCycleTime              cycleTime             = (SensorCapacitiveTouchCAP1188::StandbyCycleTime)atoi(str.TokenAtIdx(4, ' '));
+            
+            cap.StandbyConfigure(avgOrSum, samplesPerMeasurement, sampleTime, cycleTime);
         }
+    });
 
-        touchedLast = touched;
 
-        PAL.Delay(100);
-    }
+/*
+
+Test routine
+
+pstandby
+sconf 0 0 3 3
+senable 128
+ssens 7
+start
+
+*/
+
+
+    
+    shell.Start();
+
+    
+    Log("Running");
+    evm.MainLoop();
 }
 
-void loop() {}
+
+
+void loop(){}
+
 
 
 
