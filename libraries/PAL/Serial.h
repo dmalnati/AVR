2220cbf41e62a096f6afdb9ce1996f32681fc4b7
U-DUSTY\doug <douglas.malnati@gmail.com>
2019-05-20 00:11:05 -0400

    AVR Serial shuffled code to avoid circular dependency

--- libraries/PAL/Serial.h
+++ libraries/PAL/Serial.h
@@ -2,9 +2,10 @@
 #define __SERIAL_H__
 
 
+#include <math.h>
+#include <string.h>
 #include <util/atomic.h>
 
-#include "PAL.h"
 #include "Container.h"
 #include "StreamWindow.h"
 
@@ -12,100 +13,9 @@
 class Serial0
 {
 public:
-
-    void Start(uint32_t baud)
-    {
-        /*        
-         * The USART has to be initialized before any communication can take
-         * place.
-         * 
-         * The initialization process normally consists of setting the
-         * baud rate, setting frame format and enabling the Transmitter or the
-         * Receiver depending on the usage. 
-         * 
-         * For interrupt driven USART operation, the Global Interrupt Flag
-         * should be cleared (and interrupts globally disabled) when doing the
-         * initialization.     
-         */
-
-        ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-        {
-            // Stop any ongoing operation, leave in known good inert state
-            Stop();
-            
-            // Power up USART
-            PAL.PowerUpSerial0();
-            
-            // Calculate register values for given baud according to spec
-            uint16_t UBRR = (uint16_t)((PAL.GetOscillatorFreq() / 16 / baud) - 1);
-            
-            // Modified example init from pdf p. 176
-            
-            /* Set baud rate */
-            UBRR0H = (uint8_t)(UBRR >> 8);
-            UBRR0L = (uint8_t)UBRR;
-            
-            /* Enable receiver ISR, receiver, and transmitter */
-            UCSR0B = (1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0);
-            
-            /* Set frame format: 8N1 - 8data, no parity, 1stop bit */
-            UCSR0C = (0 << UCSZ02) | (1 << UCSZ01) | (1 << UCSZ00);
-        }
-    }
     
-    void Stop()
-    {
-        ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-        {
-            /*
-             * Before doing a re-initialization with changed baud rate or frame
-             * format, be sure that there are no ongoing transmissions during the
-             * period the registers are changed. The TXCn Flag can be used to check
-             * that the Transmitter has completed all transfers, and the RXC Flag
-             * can be used to check that there are no unread data in the receive
-             * buffer. Note that the TXCn Flag must be cleared before each
-             * transmission (before UDRn is written) if it is used for this purpose.
-             */
-            
-            /* 
-             * The disabling of the Transmitter (setting the TXEN to zero) will
-             * not become effective until ongoing and pending transmissions are
-             * completed, i.e., when the Transmit Shift Register and
-             * Transmit Buffer Register do not contain data to be transmitted. 
-             */
-
-            // So just disable right away
-             
-            /* 
-             * If the Receiver is disabled (RXENn = 0), the receive buffer will
-             * be flushed and consequently the RXCn bit will become zero
-             */
-            
-            // So no need to check for and discard unread data, it's automatic
-            
-            // Disable RX / TX
-            // Notably all other bits are appropriate to set to 0 here as well.
-            // This includes disabling the interrupt-enable bit for RX.
-            UCSR0B = 0;
-            
-            
-            // Check any ongoing transfers complete, wait for completion.
-            //
-            // This handles the situation where the shift-register is still
-            // clocking out a byte copied from the buffer.
-            // 
-            // Not obviously required, but does leave the serial interface in
-            // a well-known state, and at basically no cost, is worth doing.
-            if (hasWrittenAtLeastOneByte_)
-            {
-                while (!(UCSR0A & (1 << TXC0)));
-            }
-            
-            // Cut power to USART
-            PAL.PowerDownSerial0();
-        }
-        
-    }
+    void Start(uint32_t baud);
+    void Stop();
     
     void Write(uint8_t b)
     {
