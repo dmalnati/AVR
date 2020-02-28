b6c337a3bb5db8c7f03c152180d89c01c5ed95eb
unknown <douglas.malnati@gmail.com>
2020-02-27 22:43:49 -0500

    adding temp sensor SensorTemperatureMCP9808

---
+++ libraries/Peripheral/SensorTemperatureMCP9808.h
@@ -0,0 +1,267 @@
+#ifndef __SENSOR_TEMPERATURE_MCP9808__
+#define __SENSOR_TEMPERATURE_MCP9808__
+
+
+#include "I2C.h"
+#include "PAL.h"
+
+
+// https://cdn-shop.adafruit.com/datasheets/MCP9808.pdf
+
+
+class SensorTemperatureMCP9808
+{
+protected:
+    static const uint8_t ADDR = 0x18;
+
+public:
+
+    SensorTemperatureMCP9808(uint8_t addr = ADDR)
+    : addr_(addr)
+    {
+        // Nothing to do
+    }
+
+    void Init()
+    {
+        
+    }
+
+
+    void Reset()
+    {
+
+    }
+
+    void GetMeasurement()
+    {
+
+    }
+
+    struct Configuration
+    {
+        uint8_t tHyst;
+        uint8_t shdn;
+        uint8_t critLock;
+        uint8_t winLock;
+        uint8_t intClear;
+        uint8_t alertStat;
+        uint8_t alertCntl;
+        uint8_t alertSel;
+        uint8_t alertPol;
+        uint8_t alertMod;
+    };
+
+    Configuration GetConfiguration()
+    {
+        uint16_t regVal;
+        I2C.ReadRegister16(addr_, (uint8_t)RegisterPointer::CONFIG, regVal);
+
+        Configuration cfg;
+        cfg.tHyst     = (regVal & 0b0000011000000000) >> 9;
+        cfg.shdn      = (regVal & 0b0000000100000000) >> 8;
+        cfg.critLock  = (regVal & 0b0000000010000000) >> 7;
+        cfg.winLock   = (regVal & 0b0000000001000000) >> 6;
+        cfg.intClear  = (regVal & 0b0000000000100000) >> 5;
+        cfg.alertStat = (regVal & 0b0000000000010000) >> 4;
+        cfg.alertCntl = (regVal & 0b0000000000001000) >> 3;
+        cfg.alertSel  = (regVal & 0b0000000000000100) >> 2;
+        cfg.alertPol  = (regVal & 0b0000000000000010) >> 1;
+        cfg.alertMod  = (regVal & 0b0000000000000001) >> 0;
+
+        return cfg;
+    }
+
+    struct Measurement
+    {
+        uint16_t manufacturerId;
+        uint8_t deviceId;
+        uint8_t deviceRevision;
+        Configuration cfg;
+        int8_t sensorResolution;
+        int8_t tempF;
+        int8_t tempC;
+        int8_t tempAlertUpper;
+        int8_t tempAlertLower;
+        int8_t tempCritical;
+    };
+
+    uint8_t GetMeasurement(Measurement *m)
+    {
+        uint8_t retVal = 0;
+
+        if (m)
+        {
+            // Check that communication is working by confirming the
+            // manufacturer id matches the specification.
+            m->manufacturerId = GetManufacturerId();
+
+            if (m->manufacturerId == 0x0054)
+            {
+                retVal = 1;
+
+                m->deviceId = GetDeviceId();
+                m->deviceRevision = GetDeviceRevision();
+
+                m->cfg = GetConfiguration();
+                m->sensorResolution = GetSensorResolution();
+                m->tempF = GetTempF();
+                m->tempC = GetTempC();
+                m->tempAlertUpper = GetTempAlertUpper();
+                m->tempAlertLower = GetTempAlertLower();
+                m->tempCritical = GetTempCritical();
+            }
+        }
+
+        return retVal;
+    }
+    int8_t GetTempC()
+    {
+        uint16_t regVal = 0;
+        I2C.ReadRegister16(addr_, (uint8_t)RegisterPointer::TEMP, regVal);
+
+        return GetTempFromRegisterVal(regVal, 4);
+    }
+
+    int8_t GetTempF()
+    {
+        return (GetTempC() * 9.0 / 5.0) + 32;
+    }
+
+    int8_t GetTempAlertUpper()
+    {
+        uint16_t regVal = 0;
+        I2C.ReadRegister16(addr_, (uint8_t)RegisterPointer::ALERT_UPPER, regVal);
+
+        return GetTempFromRegisterVal(regVal, 2);
+    }
+
+    int8_t GetTempAlertLower()
+    {
+        uint16_t regVal = 0;
+        I2C.ReadRegister16(addr_, (uint8_t)RegisterPointer::ALERT_LOWER, regVal);
+
+        return GetTempFromRegisterVal(regVal, 2);
+    }
+
+    int8_t GetTempCritical()
+    {
+        uint16_t regVal = 0;
+        I2C.ReadRegister16(addr_, (uint8_t)RegisterPointer::CRITICAL_TEMP, regVal);
+
+        return GetTempFromRegisterVal(regVal, 2);
+    }
+
+    uint16_t GetManufacturerId()
+    {
+        uint16_t regVal = 0;
+        I2C.ReadRegister16(addr_, (uint8_t)RegisterPointer::MANUFACTURER_ID, regVal);
+
+        return regVal;
+    }
+
+    uint8_t GetDeviceId()
+    {
+        uint16_t regVal = 0;
+        I2C.ReadRegister16(addr_, (uint8_t)RegisterPointer::DEVICE_ID, regVal);
+
+        return (uint8_t)(regVal >> 8);
+    }
+
+    uint8_t GetDeviceRevision()
+    {
+        uint16_t regVal = 0;
+        I2C.ReadRegister16(addr_, (uint8_t)RegisterPointer::DEVICE_ID, regVal);
+
+        return (uint8_t)(regVal & 0x00FF);
+    }
+
+    uint8_t GetSensorResolution()
+    {
+        uint8_t regVal = 0;
+        I2C.ReadRegister(addr_, (uint8_t)RegisterPointer::SENSOR_RESOLUTION, regVal);
+
+        return regVal;
+    }
+
+    void SetSensorResolution(uint8_t val)
+    {
+        I2C.WriteRegister(addr_, (uint8_t)RegisterPointer::SENSOR_RESOLUTION, val);
+    }
+
+    void Sleep()
+    {
+        // Get current configuration
+        uint16_t regVal;
+        I2C.ReadRegister16(addr_, (uint8_t)RegisterPointer::CONFIG, regVal);
+
+        // Set the shutdown bit high
+        regVal |= _BV(8);
+
+        // Send the updated configuration back
+        I2C.WriteRegister16(addr_, (uint8_t)RegisterPointer::CONFIG, regVal);
+    }
+
+    void Wake()
+    {
+        // Get current configuration
+        uint16_t regVal;
+        I2C.ReadRegister16(addr_, (uint8_t)RegisterPointer::CONFIG, regVal);
+
+        // Set the shutdown bit low
+        regVal &= ~_BV(8);
+
+        // Send the updated configuration back
+        I2C.WriteRegister16(addr_, (uint8_t)RegisterPointer::CONFIG, regVal);
+    }
+
+
+private:
+
+    int8_t GetTempFromRegisterVal(uint16_t regVal, uint8_t subDegPlaces)
+    {
+        int8_t temp = (regVal & 0x0FFF) >> subDegPlaces;
+
+        if (regVal & 0x1000)
+        {
+            temp -= 256;
+        }
+
+        return temp;
+    }
+
+    enum class RegisterPointer : uint8_t
+    {
+        RESERVED = 0,
+        CONFIG,
+        ALERT_UPPER,
+        ALERT_LOWER,
+        CRITICAL_TEMP,
+        TEMP,
+        MANUFACTURER_ID,
+        DEVICE_ID,
+        SENSOR_RESOLUTION,
+    };
+
+
+private:
+
+    uint8_t addr_;
+};
+
+
+
+
+#endif  // __SENSOR_TEMPERATURE_MCP9808__
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sketches/TestSensorTemperatureMCP9808/TestSensorTemperatureMCP9808.ino
@@ -0,0 +1,148 @@
+#include "PAL.h"
+#include "SerialInput.h"
+#include "Evm.h"
+#include "SensorTemperatureMCP9808.h"
+#include "Log.h"
+
+
+
+static Evm::Instance<0,5,0> evm;
+static SerialAsyncConsoleEnhanced<20,20>  console;
+static SensorTemperatureMCP9808 sensor;
+
+
+
+void Display()
+{
+    SensorTemperatureMCP9808::Measurement m;
+
+    uint8_t retVal = sensor.GetMeasurement(&m);
+
+    Log(P("Success: "), retVal);
+
+    Log(P("manufacturerId: "), m.manufacturerId);
+    Log(P("deviceId: "), m.deviceId);
+    Log(P("deviceRevision: "), m.deviceRevision);
+    
+
+    Log(P("Configuration:"));
+    Log(P("  tHyst: "),     m.cfg.tHyst);
+    Log(P("  shdn: "),      m.cfg.shdn);
+    Log(P("  critLock: "),  m.cfg.critLock);
+    Log(P("  winLock: "),   m.cfg.winLock);
+    Log(P("  intClear: "),  m.cfg.intClear);
+    Log(P("  alertStat: "), m.cfg.alertStat);
+    Log(P("  alertCntl: "), m.cfg.alertCntl);
+    Log(P("  alertSel: "),  m.cfg.alertSel);
+    Log(P("  alertPol: "),  m.cfg.alertPol);
+    Log(P("  alertMod: "),  m.cfg.alertMod);
+
+
+    Log(P("SensorResolution: "), m.sensorResolution);
+    Log(P("TempF: "), m.tempF);
+    Log(P("TempC: "), m.tempC);
+
+    Log(P("TempAlertUpper: "), m.tempAlertUpper);
+    Log(P("TempAlertLower: "), m.tempAlertLower);
+    Log(P("TempCritical: "), m.tempCritical);
+}
+
+void setup()
+{
+    LogStart(9600);
+    Log("Starting");
+    
+    console.RegisterCommand("disp", [](char *){
+        Log(P("Displaying measurement"));
+        
+        Display();
+    });
+    
+    console.RegisterCommand("setres", [](char *cmdStr){
+        Str str(cmdStr);
+        
+        if (str.TokenCount(' ') == 2)
+        {
+            uint8_t val = atoi(str.TokenAtIdx(1, ' '));
+
+            Log(P("Setting sensor resolution to: "), val);
+            sensor.SetSensorResolution(val);
+
+            Display();
+        }
+    });
+
+    // my meter reads 0.0uA after this
+    console.RegisterCommand("sleep", [](char *){
+        Log(P("Sleeping"));
+        
+        sensor.Sleep();
+    });
+
+    // my meter reads ~170uA after this
+    console.RegisterCommand("wake", [](char *){
+        Log(P("Waking"));
+        
+        sensor.Wake();
+    });
+
+    // always zero, asleep or awake
+    // doesn't work when sleeping, as in, temperature never changes
+    console.RegisterCommand("ttemp", [](char *){
+        Log(P("Timing a temperature reading"));
+        
+        uint32_t timeNow = PAL.Millis();
+        int8_t tempF = sensor.GetTempF();
+        uint32_t timeEnd = PAL.Millis();
+        uint32_t timeDiff = timeEnd - timeNow;
+
+        Log("Duration: ", timeDiff, " ms, (temp: ", tempF, ")");
+    });
+
+    // ok, so a wake, measure, sleep doesn't work
+    // you have to wait for the duration associated with the resolution
+    // 0.5   C =  30ms
+    // 0.25  C =  65ms
+    // 0.125 C = 130ms
+    // 0.0625C = 250ms
+    // so certainly 300ms is a reasonable threshold
+    console.RegisterCommand("ttempwake", [](char *){
+        Log(P("Timing a temperature reading; sleep -> wake -> measure -> sleep"));
+        
+        uint32_t timeNow = PAL.Millis();
+        sensor.Wake();
+        PAL.Delay(500);
+        int8_t tempF = sensor.GetTempF();
+        sensor.Sleep();
+        uint32_t timeEnd = PAL.Millis();
+        uint32_t timeDiff = timeEnd - timeNow;
+
+        Log("Duration: ", timeDiff, " ms, (temp: ", tempF, ")");
+    });
+    
+    console.RegisterCommand("ttempwd", [](char *){
+        Log(P("Timing a temperature reading under watchdog; sleep -> wake -> measure -> sleep"));
+
+        PAL.WatchdogEnable(WatchdogTimeout::TIMEOUT_1000_MS);
+        
+        uint32_t timeNow = PAL.Millis();
+        sensor.Wake();
+        PAL.Delay(500);
+        int8_t tempF = sensor.GetTempF();
+        sensor.Sleep();
+        uint32_t timeEnd = PAL.Millis();
+        uint32_t timeDiff = timeEnd - timeNow;
+
+        PAL.WatchdogDisable();
+
+        Log("Duration: ", timeDiff, " ms, (temp: ", tempF, ")");
+    });
+
+    
+
+    sensor.Init();
+    console.Start();
+    evm.MainLoop();
+}
+
+void loop() {}
