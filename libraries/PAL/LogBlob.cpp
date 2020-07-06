08179d0e3d1776eaadc41f12250f11d616899726
unknown <douglas.malnati@gmail.com>
2020-07-06 18:27:54 -0400

    LogBlob breakout header from implementation

---
+++ libraries/PAL/LogBlob.cpp
@@ -0,0 +1,111 @@
+#include <ctype.h>
+#include <stdio.h>
+
+#include "LogBlob.h"
+
+
+void LogBlob(uint8_t  *buf,
+             uint16_t  bufSize,
+             uint8_t   showBin,
+             uint8_t   showHex)
+{
+    char bufSprintf[9];
+    
+    uint16_t bufSizeRemaining = bufSize;
+    uint16_t byteCount        = 0;
+    
+    while (bufSizeRemaining)
+    {
+        sprintf(bufSprintf, "%08X", byteCount);
+        
+        // Print byte start
+        LogNNL(P("0x"));
+        LogNNL(bufSprintf);
+        LogNNL(P(": "));
+        
+        // Calculate how many real vs pad bytes to output
+        uint8_t realBytes = (byteCount + 8 > bufSize) ?
+                            (bufSize - byteCount)     :
+                            8;
+        uint8_t padBytes = 8 - realBytes;
+        
+        // Print hex
+        if (showHex)
+        {
+            for (uint8_t i = 0; i < realBytes; ++i)
+            {
+                uint8_t b = buf[byteCount + i];
+                
+                sprintf(bufSprintf, "%02X", b);
+                LogNNL(bufSprintf);
+                LogNNL(' ');
+            }
+            
+            for (uint8_t i = 0; i < padBytes; ++i)
+            {
+                LogXNNL(' ', 3);
+            }
+            
+            LogNNL(P("| "));
+        }
+        
+        // Print binary
+        if (showBin)
+        {
+            for (uint8_t i = 0; i < realBytes; ++i)
+            {
+                uint8_t b = buf[byteCount + i];
+                
+                for (uint8_t j = 0; j < 8; ++j)
+                {
+                    LogNNL((b & 0x80) ? 1 : 0);
+                    
+                    b <<= 1;
+                }
+                
+                LogNNL(' ');
+            }
+            
+            for (uint8_t i = 0; i < padBytes; ++i)
+            {
+                LogXNNL(' ', 9);
+            }
+            
+            LogNNL(P("| "));
+        }
+        
+        // Print visible
+        for (uint8_t i = 0; i < realBytes; ++i)
+        {
+            char b = buf[byteCount + i];
+            
+            if (isprint(b))
+            {
+                LogNNL(b);
+            }
+            else
+            {
+                LogNNL('.');
+            }
+        }
+
+        LogNL();
+        
+        // Account for used data
+        byteCount += realBytes;
+        bufSizeRemaining -= realBytes;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
